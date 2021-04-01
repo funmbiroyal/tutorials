bb0da6155d436f0782ad6fa752914184487eb282
Grace <funmibiajadi@gmail.com>
2021-04-01 23:53:08 +0100

    HeartRateTest

--- test/Tdd/HeartRatesTest.java
+++ test/Tdd/HeartRatesTest.java
@@ -1,25 +1,25 @@
-//package Tdd;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//
-//public class HeartRatesTest {
-//    @Test
-//    void HeartRate_ProfileCanBeCreatedTest() {
-//        HeartRates heartRates = new HeartRates(C);
-//        assertNotNull(heartRates);
-//    }
-//
-//    @Test
-//    void firstName_CanBeCreatedTest() {
-//        HeartRates heartRates = new HeartRates();
-//        heartRates.setFirstname();
-//        assertEquals(Grace,heartRates.getFirstname());
-//
-//    }
-//
-//
-//    }
-//}
+package Tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class HeartRatesTest {
+    @Test
+    void HeartRate_ProfileCanBeCreatedTest() {
+        HeartRates heartRates = new HeartRates(C);
+        assertNotNull(heartRates);
+    }
+
+    @Test
+    void firstName_CanBeCreatedTest() {
+        HeartRates heartRates = new HeartRates();
+        heartRates.setFirstname();
+        assertEquals(Grace,heartRates.getFirstname());
+
+    }
+
+
+    }
+}
