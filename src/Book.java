2b93c95bfa27ad3bc1a36a700a867fe2a4a1ed31
Grace <funmibiajadi@gmail.com>
2021-04-05 18:21:28 +0100

    Enum Book

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public enum Book {
+public enum Book {
+    SE("software Engineering","2015" ),
+    JP("java programming","2016"),
+    PP("python programming","2017"),
+    WA("web application","2018");
+
+    private final String bookName;
+    private final String copyrightYear;
+
+    Book(String bookName, String copyrightYear) {
+        this.bookName = bookName;
+        this.copyrightYear = copyrightYear;
+    }
+    public String getBookName(){
+        return bookName;
+    }
+    public String getCopyrightYear(){
+        return copyrightYear;
+    }
 }
