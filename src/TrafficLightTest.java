cc69c9ee844c6545199c9eadc4ae4a1c94b84c11
Grace <funmibiajadi@gmail.com>
2021-04-05 19:44:05 +0100

    TrafficLightTest,enum types

--- src/TrafficLightTest.java
+++ src/TrafficLightTest.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class TrafficLightTest {
+import java.util.EnumSet;
+
+public class TrafficLightTest {
+    public static void main(String[] args) {
+        System.out.println("The Traffic light enums are :");
+        for(TrafficLight trafficLight : TrafficLight.values())
+            System.out.printf("%-25s%-20s%s%n" ,trafficLight ,trafficLight.getEnumConstant(),trafficLight.getDuration());
+        System.out.println();
+        System.out.println("The ranges without enums are as follows");
+        for(TrafficLight trafficLight : EnumSet.range(TrafficLight.STOP,TrafficLight.G0))
+        System.out.printf("%-25s%-20s%n",trafficLight.getEnumConstant(),trafficLight.getDuration());
+
+    }
 }
