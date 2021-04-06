4b166bb1de1c62fd9f5173017006bbdb709bfe9c
Grace <funmibiajadi@gmail.com>
2021-04-06 23:49:09 +0100

    Sales calculator

---
+++ src/CalculatingSales.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CalculatingSales {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("You are welcome to our Soap mart!");
+        System.out.println("Here are our product numbers and prices");
+        String message = """
+                For product 1, the price is , $2.98,
+                 For product 2, the price is , $4.50,
+                 For product 3, the price is , $9.98,
+                 For product 4, the price is , $4.49,
+                 For product 5, the price is , $6.87,
+                """;
+        System.out.println(message);
+
+
+
+        System.out.println("Enter your product Number : ");
+        int userMessage = scanner.nextInt();
+        System.out.println("Enter the price : ");
+        int userMessage2 = scanner.nextInt();
+        for (int i = 0; i < 3; ) {
+
+        i++;
+    }
+
+    }
+}
---
+++ src/Encryption.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class EncryptionTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Enter your four digit numbers : ");
+        int userDigit = scanner.nextInt();
+    }
+}
