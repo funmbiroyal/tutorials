38baac73922c358c252bf4de9de7bfe92ce55550
Grace <funmibiajadi@gmail.com>
2021-04-09 19:25:46 +0100

    Separate digits

---
+++ src/DigitSeparation.java
@@ -0,0 +1,15 @@
+public class DigitSeparation {
+    public static void main(String[] args) {
+        int userInput = 3456;
+        int firstDigit = userInput/1000;
+        int userInput2 = userInput%1000;
+        int secondDigit = userInput2/100;
+        int userInput3 = userInput2%100;
+        int thirdDigit = userInput3/10;
+        int userInput4 = userInput3%10;
+        int fourthDigit = userInput4/1;
+
+        System.out.println(firstDigit + " " + secondDigit+ " " + thirdDigit+ " " + fourthDigit);
+    }
+    }
+
