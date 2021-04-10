6997f2ff9eb918b8daedd9b99510908ebf6ff6f8
Grace <funmibiajadi@gmail.com>
2021-04-10 23:33:07 +0100

    SalesCommission Unit Test

--- test/Tdd/SalesCommissionTest.java
+++ test/Tdd/SalesCommissionTest.java
@@ -1,2 +1,46 @@
-package Tdd;public class SalesCommissionTest {
+package Tdd;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SalesCommissionTest {
+    SalesCommission salesCommission;
+
+    @BeforeEach
+    @Test
+    void startEachTestWith() {
+        salesCommission = new SalesCommission();
+    }
+
+    @Test
+    void salesCommissionCanBeCreated() {
+        SalesCommission salesCommission = new SalesCommission();
+        assertNotNull(salesCommission);
+    }
+
+    @Test
+    void userCanEnterInputInIntegers() {
+        SalesCommission salesCommission = new SalesCommission();
+        salesCommission.enterInput(230.00);
+        assertEquals(230.00, salesCommission.getInput());
+    }
+
+    @Test
+    void percentageOfSalesCanBeCalculated() {
+        salesCommission.enterInput(230.00);
+        assertEquals(20.7, salesCommission.getPercentageOfAmountOfItemSold());
+    }
+
+    @Test
+    void percentageCanBeAddedWithTheWeeklyEarning() {
+        salesCommission.enterInput(230.00);
+        salesCommission.getPercentageOfAmountOfItemSold();
+        assertEquals(220.7,salesCommission.getTotalEarning());
+
+
+    }
 }
+
+
