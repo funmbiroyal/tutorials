856ef65803fc9907bbc30ca92be7c521c233c48b
Grace <funmibiajadi@gmail.com>
2021-04-12 23:50:07 +0100

    Employee Main Method

--- src/EmployeeMain.java
+++ src/EmployeeMain.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class EmployeeMain {
+import Tdd.Employee;
+
+public class EmployeeMain {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Jane","Brown",0052);
+        System.out.println("Employee Information");
+        System.out.printf("The first name of the employee is %s%n ",employee.getFirstName());
+        System.out.printf("The last name of the employee is %s%n " ,employee.getLastName());
+        System.out.printf("The social security number of the employee is %d%n ",employee.getSocialSecurityNumber());
+        System.out.println();
+        System.out.printf("Updated employee information obtained by toString %s%n" , employee);
+
+    }
+
 }
