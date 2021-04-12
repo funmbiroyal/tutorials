96884a9286a16f8100434f86ebf4f577d0ec4ad2
Grace <funmibiajadi@gmail.com>
2021-04-12 23:51:43 +0100

    Sub-Class

--- src/CommissionPlusEmployee.java
+++ src/CommissionPlusEmployee.java
@@ -1,7 +1,20 @@
-public class CommissionEmployee {
-    private double commissionSalary;
-    public CommissionEmployee(double commissionSalary) {
+import Tdd.Employee;
 
-       this.commissionSalary = commissionSalary;
+public class CommissionPlusEmployee extends Employee {
+    private double commissionSalary;
+    public CommissionPlusEmployee(String firstName, String lastName, int socialSecurityNumber,double commissionSalary)
+    {
+        super(firstName,lastName,socialSecurityNumber);
+        if(commissionSalary <= 0.00)
+            throw new IllegalArgumentException("Invalid commission Salary, enter a valid integer!");
+        this.commissionSalary = commissionSalary;
+    }
+    public void setCommissionSalary(double commissionSalary){
+        if(commissionSalary <= 0.00)
+            throw new IllegalArgumentException("Please enter a valid integer");
+        this.commissionSalary = commissionSalary;
+    }
+    public double getCommissionSalary(){
+        return commissionSalary;
     }
 }
