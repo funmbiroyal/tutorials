a2427b5e9a74ce04fe7ec763d42cb029d0a4b4d3
Grace <funmibiajadi@gmail.com>
2021-04-13 15:49:59 +0100

    Main method

--- src/HypotenuseMainMethod.java
+++ src/HypotenuseMainMethod.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class HypotenuseMainMethod {
+import java.util.Scanner;
+
+public class HypotenuseMainMethod {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Enter your value for side 1: ");
+        double side1 = scanner.nextDouble();
+        System.out.println("Enter your value for side 2: ");
+        double side2 = scanner.nextDouble();
+        double hypotenuse = Math.sqrt(Math.pow(side1,2)+Math.pow(side2,2));
+        System.out.println("The hypotenuse is " + hypotenuse );
+    }
 }
