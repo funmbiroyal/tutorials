1d35d66b5ede1e3618f751b143b273c2243c05d0
Grace <funmibiajadi@gmail.com>
2021-04-13 15:48:33 +0100

    Hypotenuse unit testing

--- test/Tdd/HypotenuseTest.java
+++ test/Tdd/HypotenuseTest.java
@@ -1,2 +1,30 @@
-package Tdd;public class HypotenuseTest {
+package Tdd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class HypotenuseTest {
+    Hypotenuse hypotenuse;
+    @Test
+    @BeforeEach
+    void startAllTestWith(){
+        hypotenuse = new Hypotenuse ();
+    }
+    @Test
+    void canCreateHypotenuse(){
+        assertNotNull(hypotenuse);
+    }
+    @Test
+    void cannotTakeZeroOrNegativeValue(){
+        hypotenuse.enterValue(-2);
+        assertEquals(0,hypotenuse.getValue());
+    }
+    @Test
+    void canCalculateHypotenuse(){
+        hypotenuse.calculateHypotenuse(3.0, 4.0);
+        assertEquals(5.0, hypotenuse.getHypotenuse());
+    }
 }
