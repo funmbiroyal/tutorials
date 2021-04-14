fc173fd38985f8db5244ce6b521d944711d65046
Grace <funmibiajadi@gmail.com>
2021-04-14 18:16:56 +0100

    Guess number unit test

--- test/Tdd/GuessTheNumberTest.java
+++ test/Tdd/GuessTheNumberTest.java
@@ -1,2 +1,31 @@
-package Tdd;public class GuessTheNumberTest {
+package Tdd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class GuessTheNumberTest {
+    GuessTheNumber guessTheNumber;
+    @Test
+    @BeforeEach
+    void startEachTestWith(){
+        guessTheNumber = new GuessTheNumber();
+    }
+    @Test
+    void GuessTheNumberCanBeCreated(){
+        assertNotNull(guessTheNumber);
+    }
+    @Test
+    void canNotEnterNegativeIntegerByTheUser(){
+        guessTheNumber.enterInput(-1);
+        assertEquals(0,guessTheNumber.getInput());
+    }
+    @Test
+    void canOnlyEnterInputBetweenOne_to_OneThousand(){
+        guessTheNumber.enterInput(1001);
+        assertEquals(0,guessTheNumber.getInput());
+
+    }
 }
