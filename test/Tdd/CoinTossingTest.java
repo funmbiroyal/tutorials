0a2dbf1c28ccf46e85de49342b6894159c717855
Grace <funmibiajadi@gmail.com>
2021-04-14 18:15:47 +0100

    Coin tossing unit test

---
+++ test/Tdd/CoinTossingTest.java
@@ -0,0 +1,29 @@
+package Tdd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static Tdd.CoinState.HEAD;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CoinTossingTest {
+    CoinTossing coinTossing;
+    @Test
+    @BeforeEach
+    void startEachTestWith(){
+
+        coinTossing = new CoinTossing();
+    }
+    @Test
+    void canCreateCoinTossing(){
+
+        assertNotNull(coinTossing);
+    }
+    @Test
+    void canFlipCoin(){
+        coinTossing.flipCoin();
+        coinTossing.flipCoin();
+
+        assertEquals(HEAD,coinTossing.getCurrentCoinState());
+    }
+}
