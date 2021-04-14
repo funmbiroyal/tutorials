886a9c1c5505d4cd280869b400266f910b8331b6
Grace <funmibiajadi@gmail.com>
2021-04-14 18:19:11 +0100

    Guess number

--- src/Tdd/GuessTheNumber.java
+++ src/Tdd/GuessTheNumber.java
@@ -1,2 +1,15 @@
-package Tdd;public class GuessTheNumber {
+package Tdd;
+
+public class GuessTheNumber {
+    private int input;
+    private int guess;
+
+    public void enterInput(int input) {
+        if(input >0&& input <=1000)
+            this.input = input;
+    }
+
+    public int getInput() {
+        return input;
+    }
 }
