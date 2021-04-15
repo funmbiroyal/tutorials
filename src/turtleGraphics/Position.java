9f381fd1e2ec1c413876da2b6837ae7c98b2a73c
Grace <funmibiajadi@gmail.com>
2021-04-15 18:29:31 +0100

    Position class

--- src/turtleGraphics/Position.java
+++ src/turtleGraphics/Position.java
@@ -1,2 +1,33 @@
-package turtleGraphics;public class Position {
+package turtleGraphics;
+
+public class Position {
+    private int rowPosition;
+    private int columnPosition;
+
+    public Position(int rowPosition, int columnPosition) {
+        this.rowPosition = rowPosition;
+        this.columnPosition = columnPosition;
+    }
+
+    @Override
+    public String toString() {
+        String toString = "Position\n";
+        toString += "Current Row Position: " + rowPosition;
+        toString += "\nCurrent Column Position: " + columnPosition;
+        return toString;
+    }
+    @Override
+    public  boolean equals(Object anotherPosition) {
+
+        if (anotherPosition.getClass() != this.getClass()) {
+            return false;
+        }
+        Position convertedPosition = (Position) anotherPosition;
+        boolean columnsAreEqual = convertedPosition.columnPosition  == this.columnPosition ;
+        boolean rowsAreEqual = convertedPosition.rowPosition  == this.rowPosition ;
+          return columnsAreEqual && rowsAreEqual;
+    }
+    public void increaseColumnPositionBy(int numberOfSteps) {
+        columnPosition += numberOfSteps;
+    }
 }
