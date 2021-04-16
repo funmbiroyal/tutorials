2deb671232dabc87470e12ea899689b57dfee96e
Grace <funmibiajadi@gmail.com>
2021-04-16 23:42:48 +0100

    credit limit calculator unit test

--- test/CreditLimitCalculatorTest.java
+++ test/CreditLimitCalculatorTest.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class CreditLimitCalculatorTest {
+import Tdd.CreditLimitCalculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CreditLimitCalculatorTest {
+    CreditLimitCalculator creditLimitCalculator;
+    @BeforeEach
+    void StartEachTestWith(){
+        creditLimitCalculator = new CreditLimitCalculator();
+    }
+    @Test
+    void canCreateCreditLimitCalculator(){
+
+        assertNotNull(creditLimitCalculator);
+    }
+    @Test
+    void canCalculateNewBalance(){
+        creditLimitCalculator.setBeginningBalance(100);
+        creditLimitCalculator.setCreditLimits(200);
+        creditLimitCalculator.setCharges(300);
+        creditLimitCalculator.calculateNewBalance();
+        assertEquals(200,creditLimitCalculator.getNewBalance());
+        }
+        @Test
+        void canNotSetNegativeValues(){
+        creditLimitCalculator.setBeginningBalance(-100);
+        creditLimitCalculator.setCreditLimits(-200);
+        creditLimitCalculator.setCharges(-300);
+        creditLimitCalculator.calculateNewBalance();
+        assertEquals(0, creditLimitCalculator.getNewBalance());
+        }
+        @Test
+    void canCheckCreditLimit(){
+            creditLimitCalculator.setBeginningBalance(1000);
+            creditLimitCalculator.setCreditLimits(200);
+            creditLimitCalculator.setCharges(300);
+            creditLimitCalculator.calculateNewBalance();
+            assertEquals("You have exceeded your credit Limits", creditLimitCalculator.checkCreditLimit());
+        }
+
 }
+
