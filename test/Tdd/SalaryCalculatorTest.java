75a2da2ec5c34b57d275cd372142f1ec099baea7
Grace <funmibiajadi@gmail.com>
2021-04-17 23:19:06 +0100

    Salary Calculator Unit Test

--- test/Tdd/SalaryCalculatorTest.java
+++ test/Tdd/SalaryCalculatorTest.java
@@ -1,2 +1,38 @@
-package Tdd;public class SalaryCalculatorTest {
+package Tdd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SalaryCalculatorTest {
+    SalaryCalculator salaryCalculator;
+
+    @BeforeEach
+    void startAllTestWith() {
+        salaryCalculator = new SalaryCalculator();
+    }
+
+    @Test
+    void canCreateSalaryCalculatorTest() {
+        assertNotNull(salaryCalculator);
+    }
+
+    @Test
+    void canNotEnterNegativeValuesTest() {
+        salaryCalculator.enterValue(-1);
+        assertEquals(0, salaryCalculator.getValue());
+    }
+
+    @Test
+    void canCalculateGrossPayForEmployeeThatWorkedForFortyHoursTest() {
+        salaryCalculator.setHours(40);
+     assertEquals(200,salaryCalculator.getGrossPay());
+    }
+@Test
+    void canCalculateGrossPayForEmployeeThatWorkedMoreThanFortyHoursTest(){
+    salaryCalculator.calculateGrossPayWithExcess(150);
+    assertEquals(300,salaryCalculator.getGrossPayWithExcess());
+}
 }
