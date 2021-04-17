69a40330830910e79dfd5346911f8a84f8d93fa9
Grace <funmibiajadi@gmail.com>
2021-04-17 23:25:53 +0100

    Credit limit calculator

--- src/CreditLimitCalculatorMain.java
+++ src/CreditLimitCalculatorMain.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class CreditLimitCalculatorMain {
+import Tdd.CreditLimitCalculator;
+
+public class CreditLimitCalculatorMain {
+    public static void main(String[] args) {
+        CreditLimitCalculator creditLimitCalculator = new CreditLimitCalculator();
+
+        creditLimitCalculator.setBeginningBalance(100);
+        creditLimitCalculator.setCharges(300);
+        creditLimitCalculator.setCreditLimits(200);
+
+        System.out.println("Your new balance is "+ creditLimitCalculator.getNewBalance());
+        System.out.println(creditLimitCalculator.checkCreditLimit());
+        System.out.println();
+
+        creditLimitCalculator.setBeginningBalance(1000);
+        creditLimitCalculator.setCharges(300);
+        creditLimitCalculator.setCreditLimits(200);
+        System.out.println("Your new balance is "+ creditLimitCalculator.getNewBalance());
+        System.out.println(creditLimitCalculator.checkCreditLimit());
+
+    }
 }
