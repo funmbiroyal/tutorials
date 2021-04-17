ab95e69eed1d41f6b5a09cfa4e9dce65f2bbc21c
Grace <funmibiajadi@gmail.com>
2021-04-17 23:23:46 +0100

    Salary calculator main method

--- src/SalaryCalculatorMain.java
+++ src/SalaryCalculatorMain.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class SalaryCalculatorMain {
+import Tdd.SalaryCalculator;
+
+import java.util.Scanner;
+
+public class SalaryCalculatorMain {
+    public static void main(String[] args) {
+        SalaryCalculator salaryCalculator = new SalaryCalculator();
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i <= 3; i++) {
+
+
+            System.out.println("Enter numbers of hours worked last week");
+            int userHours = scanner.nextInt();
+            System.out.println("Our salary for 40hrs is " + salaryCalculator.getGrossPay());
+            if (userHours == 40) {
+                System.out.println("Your pay is " + salaryCalculator.getGrossPay());
+            } else if (userHours > 40) {
+                salaryCalculator.calculateGrossPayWithExcess(userHours);
+                System.out.println("Your pay is " + salaryCalculator.getGrossPayWithExcess());
+            }
+        }
+    }
 }
