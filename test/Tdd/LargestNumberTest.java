d1d1508cb9ed7e299780fbc18e6dce2389f619cf
Grace <funmibiajadi@gmail.com>
2021-04-18 16:33:14 +0100

    Largest number unit test

--- test/Tdd/LargestNumberTest.java
+++ test/Tdd/LargestNumberTest.java
@@ -1,2 +1,29 @@
-package Tdd;public class FindTheLargestNumberTest {
+package Tdd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class LargestNumberTest {
+    LargestNumber largestNumber;
+    @BeforeEach
+    void startEachTestWith(){
+        largestNumber = new LargestNumber();
+    }
+    @Test
+    void canCreateLargestNumberTest(){
+        assertNotNull(largestNumber);
+    }
+    @Test
+    void userCanNotEnterNonNegativeIntegerTest(){
+        largestNumber.enterInput(-1);
+        assertEquals(0,largestNumber.getInput());
+    }
+    @Test
+    void canCalculateTheLargestNumber(){
+        largestNumber.calculateLargestNumber(34,303,26);
+        assertEquals(303,largestNumber.getLargestNumber());
+    }
 }
