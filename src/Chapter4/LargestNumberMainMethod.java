6d43a52e6b62d8c5d0b42d2e2ab9f3d7fa7e6e43
Grace <funmibiajadi@gmail.com>
2021-04-18 16:34:43 +0100

    Largest number main method

--- src/Chapter4/LargestNumberMainMethod.java
+++ src/Chapter4/LargestNumberMainMethod.java
@@ -1,2 +1,18 @@
-package Chapter4;public class LargestNumberMainMethod {
+package Chapter4;
+
+import java.util.Scanner;
+
+public class LargestNumberMainMethod {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       int largestNumber = 0;
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println("Enter the number of units sold :");
+            int userInput = scanner.nextInt();
+            if(userInput > largestNumber)  largestNumber = userInput;
+
+
+        }
+        System.out.println("The largest number is "+ largestNumber);
+    }
 }
