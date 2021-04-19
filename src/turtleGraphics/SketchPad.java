428d0acfa1ad465522f192ad8ef43ec917eb171a
Grace <funmibiajadi@gmail.com>
2021-04-19 23:50:25 +0100

    Sketch pad

--- src/turtleGraphics/SketchPad.java
+++ src/turtleGraphics/SketchPad.java
@@ -1,2 +1,12 @@
-package turtleGraphics;public class SketchPad {
+package turtleGraphics;
+
+public class SketchPad {
+    private final int[][] floor;
+    public SketchPad(int numberOfRows, int numberOfColumns) {
+        floor = new int[numberOfRows][numberOfColumns];
+    }
+
+    public int[][] getFloor() {
+        return  floor;
+    }
 }
