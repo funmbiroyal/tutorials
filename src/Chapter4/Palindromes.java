f8f7bfe17533ddfebbafc3d9d3abd40659220840
Grace <funmibiajadi@gmail.com>
2021-04-20 23:07:07 +0100

    Palindromes chap4

--- src/Chapter4/Palindromes.java
+++ src/Chapter4/Palindromes.java
@@ -1,2 +1,25 @@
-package Chapter4;public class Palindromes {
+package Chapter4;
+
+import java.util.Scanner;
+
+public class Palindromes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a five digit number");
+        int userInput = scanner.nextInt();
+        int firstNumber = userInput/10000;
+        int firstDigit = userInput%10000;
+        int secondNumber = firstDigit/1000;
+        int secondDigit = firstDigit%1000;
+        int thirdNumber = secondDigit/100;
+        int thirdDigit = secondDigit%100;
+        int fourthNumber = thirdDigit/10;
+        int fifthNumber = thirdDigit%10;
+     if (firstNumber==fifthNumber && secondNumber==fourthNumber ) System.out.println("This is a palindrome!");
+     else
+         System.out.println("Not a palindrome!");
+
+
+        //System.out.println(firstNumber +" " +secondNumber +" "+ thirdNumber + " "+ fourthNumber+ " "+ fifthNumber);
+    }
 }
