8c83c978157c0a61fa68c0c6f1887f6e96ff9df8
Grace <funmibiajadi@gmail.com>
2021-04-21 22:40:21 +0100

    Find the minimum

---
+++ src/Chapter5/Factorial.java
@@ -0,0 +1,14 @@
+package Chapter5;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int [] array = new int[5];
+        for (int i = 1; i <= array.length ; i++) {
+
+
+
+
+
+
+    }
+}
