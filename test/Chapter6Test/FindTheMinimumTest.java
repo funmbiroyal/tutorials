c9a3646c4085a3cce87269504080703ce7130dad
Grace <funmibiajadi@gmail.com>
2021-04-22 22:10:06 +0100

    Minimum ,Unit testing

--- test/Chapter6Test/FindTheMinimumTest.java
+++ test/Chapter6Test/FindTheMinimumTest.java
@@ -1,2 +1,25 @@
-package Chapter6Test;public class FindTheMinimumTest {
+package Chapter6Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class FindTheMinimumTest {
+    FindTheMinimum findTheMinimum;
+    @BeforeEach
+    void startEachTestWithThis(){
+        findTheMinimum = new FindTheMinimum();
+    }
+    @Test
+    void canCreateFindMinimumTest(){
+        assertNotNull(findTheMinimum);
+    }
+    @Test
+    void canFindTheMinimum(){
+        findTheMinimum.calculateMinimum(45.0,6.0,36.0);
+        assertEquals(6.0,findTheMinimum.getMinimum());
+
+    }
 }
