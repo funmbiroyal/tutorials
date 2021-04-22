479d99a6b1d58ad172f0e597aba13b193e71562b
Grace <funmibiajadi@gmail.com>
2021-04-22 17:46:01 +0100

    Factorial, Modified

--- src/Chapter5/Factorial.java
+++ src/Chapter5/Factorial.java
@@ -2,9 +2,14 @@ package Chapter5;
 
 public class Factorial {
     public static void main(String[] args) {
-        int [] array = new int[5];
-        for (int i = 1; i <= array.length ; i++) {
+        System.out.printf("%s%30s%n", "values", "factorials");
+         long factorial = 1;
+         int number = 20;
+        for (int i = 2; i <=number ; i++) {
+            factorial = factorial*i;
+            System.out.printf("%d%30d%n",i,factorial);
 
+        }
 
 
 
