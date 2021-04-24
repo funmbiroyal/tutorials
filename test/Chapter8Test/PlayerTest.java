f0ef6336543c62656a49f690d496f4714f9dc13e
Grace <funmibiajadi@gmail.com>
2021-04-24 19:03:56 +0100

    Players Test

--- test/Chapter8Test/PlayerTest.java
+++ test/Chapter8Test/PlayerTest.java
@@ -1,2 +1,22 @@
-package Chapter8Test;public class PlayerTest {
+package Chapter8Test;
+
+import Chapter8.Players;
+import Chapter8.TicTacToe;
+import org.junit.jupiter.api.Test;
+
+import static Chapter8.BoardState.X;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class PlayerTest {
+
+
+
+    @Test
+    void playerXcanMove(){
+        TicTacToe ticTacToe =new TicTacToe();
+        Players playerX = new Players();
+        playerX.playerXMove(0,0);
+        assertEquals(X,ticTacToe.board[0][0]);
+    }
 }
