793fa9fd24f2249bd63d35f094d9a94e96282e98
Grace <funmibiajadi@gmail.com>
2021-04-27 20:39:03 +0100

    Payroll

--- test/Chapter10Test/PayrollTest.java
+++ test/Chapter10Test/PayrollTest.java
@@ -1,2 +1,37 @@
-package Chapter10Test;public class PayrollTest {
+package Chapter10Test;
+
+import Chapter10.Payroll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class PayrollTest {
+    Payroll payroll;
+@BeforeEach
+void startEachTestWith(){
+    payroll = new Payroll("Grace","Ola",56);
+}
+    @Test
+    void PayrollCanBeCreated(){
+        assertNotNull(payroll);
+    }
+    @Test
+    void canGetFirstName(){
+    assertEquals("Grace",payroll.getFirstname());
+    }
+    @Test
+    void canGetLastName(){
+    assertEquals("Ola",payroll.getLastName());
+    }
+    @Test
+    void canGetSocialSecurityNumber(){
+    assertEquals(56,payroll.getSecurityNumber());
+    }
+@Test
+    void canPrintToString(){
+    assertEquals(toString(),payroll.toString());
+
+}
 }
