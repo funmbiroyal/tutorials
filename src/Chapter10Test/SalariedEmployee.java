cc038e474a1e6826283d95310f5c9b59000ab846
Grace <funmibiajadi@gmail.com>
2021-04-29 17:41:12 +0100

    Salaried Employee

--- src/Chapter10Test/SalariedEmployee.java
+++ src/Chapter10Test/SalariedEmployee.java
@@ -1,2 +1,17 @@
-package Chapter10Test;public class SalariedEmployee {
+package Chapter10Test;
+
+import Tdd.Employee;
+
+public class SalariedEmployee extends Employee {
+   private final int salary;
+
+    public  SalariedEmployee(String firstName,String lastName, int socialSecurityNumber,int salary){
+        super(firstName,lastName,socialSecurityNumber);
+            if(salary < 0) throw new IllegalArgumentException("Salary must be greater than zero");
+                this.salary = salary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
 }
