c2784d042d5675c8e1888b4927383807974eedd7
Grace <funmibiajadi@gmail.com>
2021-04-29 17:45:07 +0100

    SalariedEmployee Unit test

--- test/Chapter10Test/SalariedEmployeeTest.java
+++ test/Chapter10Test/SalariedEmployeeTest.java
@@ -1,2 +1,36 @@
-package Chapter10Test;public class SalariedEmployeeTest {
+package Chapter10Test;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SalariedEmployeeTest {
+    SalariedEmployee salariedEmployee;
+    @BeforeEach
+    void startAllTestWith(){
+       salariedEmployee = new SalariedEmployee("Lara",
+               "George",5678,10000);
+    }
+    @Test
+    void salariedEmployeeCanBeCreated(){
+    assertNotNull(salariedEmployee);
+}
+    @Test
+    void CanGetFirstName (){
+    assertEquals("Lara",salariedEmployee.getFirstName());
+    }
+
+    @Test
+    void CanGetLastName (){
+        assertEquals("George",salariedEmployee.getLastName());
+    }
+    @Test
+    void CanGetSocialSecurityNumber (){
+        assertEquals(5678,salariedEmployee.getSocialSecurityNumber());
+    }
+    @Test
+    void canGetSalary(){
+        assertEquals(10000,salariedEmployee.getSalary());
+    }
 }
