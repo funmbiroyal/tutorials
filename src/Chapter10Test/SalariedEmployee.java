ed41247d2e7afcad708cdc9c527ac6a4d16c590f
Grace <funmibiajadi@gmail.com>
2021-05-01 22:07:28 +0100

    Modification

--- src/Chapter10Test/SalariedEmployee.java
+++ src/Chapter10Test/SalariedEmployee.java
@@ -4,14 +4,32 @@ import Tdd.Employee;
 
 public class SalariedEmployee extends Employee {
    private final int salary;
+   private int day;
+   private int month;
+   private int year;
 
-    public  SalariedEmployee(String firstName,String lastName, int socialSecurityNumber,int salary){
+    public  SalariedEmployee(String firstName,String lastName, int socialSecurityNumber,int salary, int day,
+                             int month, int year){
         super(firstName,lastName,socialSecurityNumber);
             if(salary < 0) throw new IllegalArgumentException("Salary must be greater than zero");
                 this.salary = salary;
+                this.day = day;
+                this.month = month;
+                this.year = year;
+
     }
 
     public int getSalary() {
+
         return salary;
     }
+
+    public int getDateOfBirth(){
+        return day / month / year;
+    }
+    @Override
+    public int earnings() {
+        return salary;
+    }
+
 }
