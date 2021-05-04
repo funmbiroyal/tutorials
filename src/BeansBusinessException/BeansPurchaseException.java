353f96d567a98e59947e3ae65bcec59932b6ac68
Grace <funmibiajadi@gmail.com>
2021-05-04 23:47:19 +0100

    Purchase Exception

--- src/BeansBusinessException/BeansPurchaseException.java
+++ src/BeansBusinessException/BeansPurchaseException.java
@@ -1,4 +1,16 @@
 package BeansBusinessException;
 
-public class BeansBusiness {
+import javax.naming.InsufficientResourcesException;
+
+public class BeansPurchaseException  {
+    public Beanss buyBeans(int amount) throws InsufficientResourcesException {
+if(amount == 100 ){
+    System.out.println("You just bought a plate of beans!");
+    return new Beanss();
+}
+else
+    throw new InsufficientResourcesException("Your money is not sufficient");
+    }
+
+
 }
