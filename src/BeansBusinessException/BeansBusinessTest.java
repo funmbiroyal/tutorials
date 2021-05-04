f59d78a377784ae1effeeb6a0cec8d7531416549
Grace <funmibiajadi@gmail.com>
2021-05-04 23:48:29 +0100

    Exception Test

--- src/BeansBusinessException/BeansBusinessTest.java
+++ src/BeansBusinessException/BeansBusinessTest.java
@@ -1,2 +1,18 @@
-package BeansBusinessException;public class BeansBusinessTest {
+package BeansBusinessException;
+
+import javax.naming.InsufficientResourcesException;
+
+public class BeansBusinessTest {
+    public static void main(String[] args) {
+
+        BeansPurchaseException purchase = new BeansPurchaseException();
+
+        try{
+           Beanss beanss = purchase.buyBeans(50) ;
+
+        }
+        catch (InsufficientResourcesException ex){
+            System.out.println(ex);
+        }
+    }
 }
