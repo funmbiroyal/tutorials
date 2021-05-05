b5a8836868d4b9a9d12885dec02aadadaf3373c2
Grace <funmibiajadi@gmail.com>
2021-05-05 23:18:28 +0100

    String palindrome

--- test/Chapter14Test/StringPalindrome.java
+++ test/Chapter14Test/StringPalindrome.java
@@ -1,2 +1,27 @@
-package Chapter14Test;public class StringPalindrome {
+package Chapter14Test;
+
+import java.util.Scanner;
+
+public class StringPalindrome {
+public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    for (int i = 1; i <= 2; i++) {
+
+        System.out.println("Enter a String Value:");
+        String userInput = scanner.next();
+        StringBuilder sb = new StringBuilder(userInput);
+
+        StringBuilder reversedUserInput = sb.reverse();
+
+        if (userInput.equals(String.valueOf(reversedUserInput))) {
+            System.out.println("This is a palindrome");
+        } else {
+            System.out.println("This is not a palindrome");
+        }
+    }
+
+}
+
 }
