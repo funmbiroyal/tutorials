936af9eaa54374ba0e9374d6acf04f699a0a2d2d
Grace <funmibiajadi@gmail.com>
2021-05-06 23:15:41 +0100

    modified beans exception

--- src/BeansBusinessException/BeansPurchaseException.java
+++ src/BeansBusinessException/BeansPurchaseException.java
@@ -1,15 +1,15 @@
 package BeansBusinessException;
 
-import javax.naming.InsufficientResourcesException;
+//import javax.naming.InsufficientResourcesException;
 
 public class BeansPurchaseException  {
-    public Beanss buyBeans(int amount) throws InsufficientResourcesException {
+    public Beanss buyBeans(int amount) throws LackOfMoneyException {
 if(amount == 100 ){
     System.out.println("You just bought a plate of beans!");
     return new Beanss();
 }
 else
-    throw new InsufficientResourcesException("Your money is not sufficient");
+    throw new LackOfMoneyException("Your money is not sufficient");
     }
 
 
