e3a4b53834e29758969a96aba1faea96436350c7
Grace <funmibiajadi@gmail.com>
2021-05-06 23:08:22 +0100

    String occurrence in a Sentence

--- src/Chapter14/WordOccurrence.java
+++ src/Chapter14/WordOccurrence.java
@@ -1,2 +1,24 @@
-package Chapter14;public class WordOccurence {
+package Chapter14;
+
+public class WordOccurrence {
+    public static void main(String[] args) {
+        String sentence = "andhepassedoverandoveragain";
+        String word = "he";
+        int count = 0;
+        for (int i = 0; i < sentence.length(); i++) {
+            if (sentence.regionMatches(i, word, 0, word.length())) {
+                count++;
+
+
+
+
+        }
+
+    }
+        System.out.println(count);
+
+
+
+
+}
 }
