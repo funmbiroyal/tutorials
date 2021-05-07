5403a72eaa92db6cb530b890b1d6834853cda6e9
Grace <funmibiajadi@gmail.com>
2021-05-07 18:37:54 +0100

    Regex unit testing

--- test/Chapter14Test/RegexTest.java
+++ test/Chapter14Test/RegexTest.java
@@ -1,2 +1,43 @@
-package Chapter14Test;public class ValidateUserInputUsingRegexTest {
+package Chapter14Test;
+
+import Chapter14.Regex;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RegexTest {
+    @Test
+    void regexCanBeCreatedTest(){
+        Regex regex = new Regex();
+        assertNotNull(regex);
+    }
+    @Test
+    void canValidateFirstname(){
+        assertEquals(true,Regex.validateFirstname("Grace"));
+    }
+    @Test
+    void  canValidateLastNameTest(){
+        assertEquals(true,Regex.validateLastname("Peter-ross"));
+    }
+    @Test
+    void canValidateAddressTest(){
+     assertEquals(true,Regex.validateAddress("5244 Herbert Macaulay"));
+    }
+    @Test
+    void canValidateCityTest(){
+        assertEquals(true,Regex.validateCity("Sabo Yaba"));
+    }
+    @Test
+    void canValidateStateTest(){
+        assertEquals(true,Regex.validateState("Lagos"));
+    }
+    @Test
+    void canValidateZipTest(){
+        assertEquals(true,Regex.validateZip("346"));
+    }
+    @Test
+    void canValidatePhoneNumber(){
+        assertEquals(true,Regex.validatePhoneNumber("+234-632-414-56"));
+       // assertEquals(true,Regex.validatePhoneNumber("236-561-7890"));
+    }
 }
