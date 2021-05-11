61dacdb146048f75f0553e6c749fcd987b6da984
Grace <funmibiajadi@gmail.com>
2021-05-12 00:20:20 +0100

    Pii Series

--- src/Chapter5/PiiSeries.java
+++ src/Chapter5/PiiSeries.java
@@ -1,2 +1,20 @@
-package Chapter5;public class PiiSeries {
+package Chapter5;
+
+public class PiiSeries {
+    public static void main(String[] args) {
+        double pii = 0;
+        System.out.printf("%s%10s%n", "Term", "Value");
+        for (int i = 1; i < 200_000; i++) {
+            if (i == 1) {
+                pii = 4;
+            }
+            else if (i % 2 == 0) {
+                pii = pii - (double) 4 / (i + (i - 1));
+            }
+            else {
+                pii = pii + (double) 4 / (i + (i - 1));
+            }
+            System.out.printf("%d%20f%n", i, pii);
+        }
+    }
 }
