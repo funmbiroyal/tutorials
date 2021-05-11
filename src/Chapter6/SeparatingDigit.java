546312febe5921864aacfcab61194f8a250ec937
Grace <funmibiajadi@gmail.com>
2021-05-12 00:18:56 +0100

    Separating Digit

--- src/Chapter6/SeparatingDigit.java
+++ src/Chapter6/SeparatingDigit.java
@@ -1,5 +1,41 @@
 package Chapter6;
 
-public class Separingdigit {
-    
+import java.util.Scanner;
+
+public class SeparatingDigit {
+
+    public static int getQuotient(int a, int b) {
+        int answer = a / b;
+        return answer;
+    }
+
+    public static int getRemainder(int a, int b) {
+        int answer = a % b;
+        return answer;
+    }
+
+
+    public static void displayDigit(int userDigit) {
+        String result = "";
+
+        while (userDigit > 0) {
+            int unit = getRemainder(userDigit, 10);
+            result = unit + " " + result;
+            userDigit = getQuotient(userDigit, 10);
+
+        }
+        System.out.println(result);
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Digit ");
+        int userDigit = scanner.nextInt();
+        if (userDigit >= 1 && userDigit <= 99999) {
+            displayDigit(userDigit);
+        }else
+            System.out.println("Numbers out of range!");
+    }
 }
