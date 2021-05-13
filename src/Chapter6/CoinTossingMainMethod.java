3b2d5da94ecda6f54174e9d4e5669dfc40b3a7cd
Grace <funmibiajadi@gmail.com>
2021-05-13 18:36:49 +0100

    Coin tossing main method

--- src/Chapter6/CoinTossingMainMethod.java
+++ src/Chapter6/CoinTossingMainMethod.java
@@ -1,2 +1,15 @@
-package Chapter6;public class CoinTossingMainMethod {
+package Chapter6;
+
+public class CoinTossingMainMethod {
+    public static void main(String[] args) {
+        CoinTossing coinTossing = new CoinTossing();
+        int chance = 10;
+        for(int i = 0; i<10; i++){
+            coinTossing.flipCoin();
+        }
+
+        System.out.println("Head appeared  " +" "+ coinTossing.getNumberOfTimesForHead() + " " + "times");
+        System.out.println("Tail  appeared " +" "+ coinTossing.getNumberOfTimesForTail() + " " + "times");
+
+    }
 }
