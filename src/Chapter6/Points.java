6e8429b644db8f15029d23c0e2531e7104fa65b6
Grace <funmibiajadi@gmail.com>
2021-05-13 18:42:55 +0100

    Qualify point

--- src/Chapter6/Points.java
+++ src/Chapter6/Points.java
@@ -1,2 +1,31 @@
-package Chapter6;public class Points {
+package Chapter6;
+
+import java.util.Scanner;
+
+public class Points {
+    private static int point;
+
+
+    public static int qualifyPoint(int average) {
+        if (average >= 90 && average <= 100) {
+            point = 4;
+        } else if (average >= 80 && average <= 89) {
+            point = 3;
+        } else if (average >= 70 && average <= 79) {
+            point = 2;
+        } else {
+            point = 0;
+        }
+        return point;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your average: ");
+        int userInput = scanner.nextInt();
+        System.out.println("Your point is " + " "+ qualifyPoint(userInput));
+    }
 }
