f3e5fb486bc7d2d527cb2eb824b0cfe53463b3b4
Grace <funmibiajadi@gmail.com>
2021-05-19 21:50:32 +0100

    Sum array

--- src/Chapter7/SumArray.java
+++ src/Chapter7/SumArray.java
@@ -1,2 +1,13 @@
-package Chapter7;public class SumArray {
+package Chapter7;
+
+public class SumArray {
+    public static void main(String[] args) {
+        int array [] = {45,34,66,10,100};
+        int total = 0;
+        for (int counter = 0; counter < array.length ; counter++) {
+            total += array[counter];
+
+        }
+        System.out.printf("sum of array in the element: %d%n ",total);
+    }
 }
