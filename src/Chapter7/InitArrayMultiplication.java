8b3d9cccb0ada2b25de82fa81702f3d0fd817320
Grace <funmibiajadi@gmail.com>
2021-05-19 21:52:10 +0100

    multiplication

--- src/Chapter7/InitArrayMultiplication.java
+++ src/Chapter7/InitArrayMultiplication.java
@@ -1,2 +1,19 @@
-package Chapter7;public class InitArrayMultiplication {
+package Chapter7;
+
+public class InitArrayMultiplication {
+    public static void main(String[] args) {
+        final int array_length = 10;
+        int [] array = new int[array_length];
+        for (int counter = 0; counter < array.length; counter++)
+
+            array[counter] = 2+2*counter;
+            System.out.printf("%s%8s%n","Value","index");
+
+
+            for (int i = 0; i < array.length; i++)
+            System.out.printf("%d%8d%n", i,array[i]);
+
+
+
+    }
 }
