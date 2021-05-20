a9cbba8ab77ffeb283ffe7f78b384be6db81505a
Grace <funmibiajadi@gmail.com>
2021-05-20 22:37:32 +0100

    Player

--- src/Chapter7/Player.java
+++ src/Chapter7/Player.java
@@ -1,2 +1,23 @@
-package Chapter7;public class Player {
+package Chapter7;
+
+import java.util.ArrayList;
+
+public class Player {
+    private String playerName;
+    private ArrayList<Card>cardGames = new ArrayList<>();
+    public Player(String player) {
+        playerName = player;
+    }
+
+    public String getPlayer() {
+        return playerName;
+    }
+
+    public void addCards(Card cardGame) {
+        cardGames.add(cardGame);
+    }
+
+    public int getNumberOfCards() {
+        return cardGames.size();
+    }
 }
