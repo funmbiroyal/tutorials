2369f9422d127cd86a9ee93d9079d0e067a5a258
Grace <funmibiajadi@gmail.com>
2021-05-20 22:46:03 +0100

    player unit test

--- test/Chapter7Test/PlayerTest.java
+++ test/Chapter7Test/PlayerTest.java
@@ -1,2 +1,28 @@
-package Chapter7Test;public class PlayerTest {
+package Chapter7Test;
+
+import Chapter7.Card;
+import Chapter7.Player;
+import Chapter7.Suit;
+import org.junit.jupiter.api.Test;
+
+import static Chapter7.Suit.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerTest {
+    @Test
+    void playerHasAnId(){
+        Player player = new Player("Henry Mike");
+        assertEquals("Henry Mike",player.getPlayer());
+
+    }
+    @Test
+    void playerHasCards(){
+        Player player = new Player("Henry Mike");
+        player.addCards(new Card(CLUB,3));
+        player.addCards(new Card(DIAMOND,6));
+        player.addCards(new Card(HEART,9));
+        player.addCards(new Card(SPADE,4));
+        player.addCards(new Card(DIAMOND,6));
+        assertEquals(5,player.getNumberOfCards());
+    }
 }
