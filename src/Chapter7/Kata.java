571c862cc0f69d7620129125f9e65696b366a94d
Grace <funmibiajadi@gmail.com>
2021-05-21 18:10:24 +0100

    Shuffle Array

--- src/Chapter7/Kata.java
+++ src/Chapter7/Kata.java
@@ -1,2 +1,20 @@
-package Chapter7;public class Kata {
+package Chapter7;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+public class Kata {
+    SecureRandom randomGenerator = new SecureRandom();
+
+    public void shuffle(int [] array) {
+        for (int i = 0; i < array.length ; i++) {
+            int generatedRandomNumber = randomGenerator.nextInt(array.length);
+            int store = array[generatedRandomNumber];
+            array[generatedRandomNumber] = array[i];
+            array[i]=store;
+
+        }
+        System.out.println(Arrays.toString(array));
+
+    }
 }
