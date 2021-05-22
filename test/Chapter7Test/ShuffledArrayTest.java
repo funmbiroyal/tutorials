6b5db5801c88d6b8423bbf9d3b1194ef91caefbb
Grace <funmibiajadi@gmail.com>
2021-05-22 23:19:38 +0100

    Unit test, sorting abd shuffling of array

--- test/Chapter7Test/ShuffledArrayTest.java
+++ test/Chapter7Test/ShuffledArrayTest.java
@@ -1,6 +1,7 @@
 package Chapter7Test;
 
 import Chapter7.Kata;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.security.SecureRandom;
@@ -9,14 +10,29 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ShuffledArrayTest {
-
+    Kata kata;
+@BeforeEach
+void startEachTestWith(){
+    kata = new Kata();
+}
 
     @Test
     void canShuffleArray() {
       int [] array = {3, 4, 6, 7, 9};
-        Kata kata = new Kata();
-        kata.shuffle(array);
+      kata.shuffle(array);
         assertNotSame(Arrays.toString(array),array);
 
+    }
+    @Test
+    void canSortArrayInAscendingOrder(){
+        int [] array = {7,6,5,3,4,1};
+        kata.sortInAscendingOrder(array);
+        assertNotSame(Arrays.toString(array),array);
+    }
+    @Test
+    void canSortArrayInDescendingOrder(){
+        int [] array = {7,9,3,1,5,8,2};
+        kata.sortInDescendingOrder(array);
+        assertNotSame(Arrays.toString(array),array);
     }
 }
