72401ae7939698370c6bd3a1dc3b6a9e347403a3
Grace <funmibiajadi@gmail.com>
2021-05-22 23:17:00 +0100

    Encrypting and decrypting

--- src/Chapter4/EncryptionPsvm.java
+++ src/Chapter4/EncryptionPsvm.java
@@ -1,2 +1,11 @@
-package Chapter4;public class EncryptionPsvm {
+package Chapter4;
+
+public class EncryptionPsvm {
+    public static void main(String[] args) {
+        Encryption encryption = new Encryption();
+        encryption.encrypt(2345);
+        System.out.println("The encrypted message is "+ " "+ encryption.getEncryptedMessage());
+        encryption.decrypt(1290);
+        System.out.println("The decrypted message is "+ " "+ encryption.getDecryptedMessage());
+    }
 }
