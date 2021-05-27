93d5bc3ba17f589a902e104eba8486e5e454fa9a
Grace <funmibiajadi@gmail.com>
2021-05-27 14:28:45 +0100

    Card ----modified

--- src/Chapter7/Card.java
+++ src/Chapter7/Card.java
@@ -6,8 +6,8 @@ public class Card {
     private final Suit suit;
     private final int value;
     public Card(Suit suit, int value) {
-        if (value <1 || value > 12){
-            throw new InvalidCardValueException("value + is not a valid vardvvalue");
+        if (value < 1 || value > 13){
+            throw new InvalidCardValueException("value + is not a valid card value");
         }
         this.suit = suit;
         this.value = value;
@@ -18,13 +18,26 @@ public class Card {
     }
 
     public String getValue() {
-        switch (this.value){
-            case 1: return "Ace";
-            case 11: return "Jack";
-            case 12: return "Queen";
-            case 13: return "King";
-            default: return value + "";
+        return switch (this.value) {
+            case 1 -> "Ace";
+            case 11 -> "Jack";
+            case 12 -> "Queen";
+            case 13 -> "King";
+            default -> value + "";
+        };
+    }
 
+    @Override
+    public String toString() {
+        return getValue() + " of " + suit.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!this.getClass().equals(obj.getClass())){
+            return false;
         }
+        Card cardToCompare = (Card) obj;
+        return this.value == cardToCompare.value || this.suit == cardToCompare.suit;
     }
 }
