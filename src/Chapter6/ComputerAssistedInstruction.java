c0abcff229bb8df5bfe42f736b3c6a9586a75adc
Grace <funmibiajadi@gmail.com>
2021-06-01 16:04:16 +0100

    CAI

--- src/Chapter6/ComputerAssistedInstruction.java
+++ src/Chapter6/ComputerAssistedInstruction.java
@@ -9,34 +9,59 @@ public class ComputerAssistedInstruction {
     private static SecureRandom secureRandom = new SecureRandom();;
     private static int firstGeneratedNumber;
     private static int secondGeneratedNumber;
+    private static  String temp;
 
     public static void generateQuestion(){
-        firstGeneratedNumber = 1 + secureRandom.nextInt(10);
-        secondGeneratedNumber = 1 + secureRandom.nextInt(10);
+        firstGeneratedNumber = 1 + secureRandom.nextInt(5);
+        secondGeneratedNumber = 1 + secureRandom.nextInt(5);
         System.out.println("How much is " + firstGeneratedNumber + " times " + secondGeneratedNumber);
+
+
+
     }
 
-    public static void answer(int answer){
-        if(answer == firstGeneratedNumber * secondGeneratedNumber){
+    public static void rightAnswer(int answer) {
+
+        if (answer == firstGeneratedNumber * secondGeneratedNumber) {
             System.out.println("Very good!");
-        }else {
-            System.out.println("No. Please try again.");
         }
-    }
+   }
+        public static  void question(){
+        temp = firstGeneratedNumber + " times " + secondGeneratedNumber;
+   }
+        public static  void wrongAnswer(int answer) {
+           if (answer != firstGeneratedNumber * secondGeneratedNumber) {
+                System.out.println("No. Please try again.");
+                question();
+                //System.out.println("How much is " + temp);
+
+
+            }
+       }
+
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int userAnswer;
-        do {
+        int userAnswer = 0;
+
+
+//        do {
+//            generateQuestion();
+//            userAnswer = scanner.nextInt();
+//            answer(userAnswer);
+//        }while (userAnswer == firstGeneratedNumber * secondGeneratedNumber);
+//       // generateQuestion();
+//        userAnswer = scanner.nextInt();
+//        answer(userAnswer);
+        for (int i = 0; i < 10 ; i++) {
             generateQuestion();
             userAnswer = scanner.nextInt();
-            answer(userAnswer);
-        }while (userAnswer == firstGeneratedNumber * secondGeneratedNumber);
-        while (userAnswer!=firstGeneratedNumber * secondGeneratedNumber);
-        generateQuestion();
-        userAnswer = scanner.nextInt();
-        answer(userAnswer);
-
+              if (userAnswer == firstGeneratedNumber * secondGeneratedNumber) {
+                  rightAnswer(userAnswer);
+              }else
+            wrongAnswer(userAnswer);
+        }
 
     }
 }
