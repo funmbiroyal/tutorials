491eb1249fe544e54fcd65e8b5d481393cd26c3c
Grace <funmibiajadi@gmail.com>
2021-06-03 23:43:50 +0100

    Tictactoe main

--- src/Chapter8/Main.java
+++ src/Chapter8/Main.java
@@ -1,2 +1,36 @@
-package Chapter8;public class Main {
+package Chapter8;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player();
+        Scanner scanner = new Scanner(System.in);
+        boolean gameOver = false;
+        while (!gameOver) {
+            try {
+                player.display();
+                System.out.println("Enter a position to play:");
+                int playerGridPosition = scanner.nextInt();
+                player.play(playerGridPosition);
+            } catch (GameOverException e) {
+                if (player.lastValuePlayedWasX) {
+                    System.out.println("O WON!");
+                } else {
+                    System.out.println("X WON");
+                }
+                gameOver = true;
+            } catch (BoardFullException e) {
+                System.out.println("Its a draw!");
+                gameOver = true;
+            }catch(AlreadyOccupiedException e){
+                System.out.println("Space has been Occupied!");
+            }
+
+        }
+    }
 }
+
+
+
+
