a7747d489db0068fea551b2de5c87a1ad15b3347
Grace <funmibiajadi@gmail.com>
2021-06-05 18:00:35 +0100

    Sales Commission main method

--- src/Chapter7/SalesCommissionMain.java
+++ src/Chapter7/SalesCommissionMain.java
@@ -1,2 +1,31 @@
-package Chapter7;public class SalesCommissionMain {
+package Chapter7;
+
+import java.util.Scanner;
+
+public class SalesCommissionMain {
+    public static void main(String[] args) {
+        SalesCommission salesCommission = new SalesCommission();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your weekly earning: ");
+
+        int userWeeklyEarning = scanner.nextInt();
+        salesCommission.verifyWeeklyEarning(userWeeklyEarning);
+
+        while (userWeeklyEarning!= 0){
+
+            System.out.println("Enter gross sales:  ");
+            int userGrossSales = scanner.nextInt();
+
+            System.out.println("Enter your weekly earning: ");
+            userWeeklyEarning = scanner.nextInt();
+            salesCommission.verifyWeeklyEarning(userWeeklyEarning);
+
+            double grossPay = salesCommission.calculateGrossPay(userWeeklyEarning,userGrossSales);
+          salesCommission.determineSalaryRange((int) grossPay);
+          //salesCommission.displayResult();
+        }
+
+    }
 }
