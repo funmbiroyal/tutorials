7227d8829a36bea1b11c9aba29650e025a720207
Grace <funmibiajadi@gmail.com>
2021-06-05 18:04:06 +0100

    Unit test

--- test/Chapter7Test/SalesCommissionTest.java
+++ test/Chapter7Test/SalesCommissionTest.java
@@ -1,2 +1,28 @@
-package Chapter7Test;public class SalesCommissionTest {
+package Chapter7Test;
+
+import Chapter7.SalesCommission;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SalesCommissionTest {
+    SalesCommission salesCommission;
+    @BeforeEach
+    void startEachTestWith(){
+        salesCommission = new SalesCommission();
+    }
+    @Test
+    void SalesCommissionCanBeCreated(){
+        assertNotNull(salesCommission);
+    }
+    @Test
+    void canCalculateGrossPay(){
+      assertEquals(650.00,salesCommission.calculateGrossPay(200,5000));
+    }
+    @Test
+    void canCreateArrayOfCounters(){
+        salesCommission.verifyWeeklyEarning(200);
+       salesCommission.determineSalaryRange(500);
+    }
 }
