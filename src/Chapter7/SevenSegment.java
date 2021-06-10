05be25cdaa48d65918514bcaf21790461318285d
Grace <funmibiajadi@gmail.com>
2021-06-10 23:13:44 +0100

    Seven segment display

--- src/Chapter7/SevenSegment.java
+++ src/Chapter7/SevenSegment.java
@@ -1,6 +1,6 @@
 package Chapter7;
 
-public class SevenSegmentDisplay {
+public class SevenSegment {
    int [][] myArray = new int[5][4];
 
    public void writeOnA(){
@@ -46,6 +46,67 @@ public class SevenSegmentDisplay {
 
         }
     }
+    public void collectValue(String value){
+        validateValue(value);
+        powerBit(value);
+        for (int i = 0; i < value.length() ; i++) {
+            if (value.charAt(i) == '1'){
+                switch (i){
+                    case 0: writeOnA();
+                    break;
+
+                    case 1 : writeOnB();
+                    break;
+
+                    case 2 : writeOnC();
+                    break;
+
+                    case 3 : writeOnD();
+                    break;
+
+                    case  4 : writeOnE();
+                    break;
+
+                    case  5 : writeOnF();
+                    break;
+
+                    case 6 : writeOnG();
+                    break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+    }
+    private void validateValue(String value) throws IllegalArgumentException,StringIndexOutOfBoundsException{
+        for (int i = 0; i < value.length(); i++) {
+            if (value.charAt(i) != '1' && value.charAt(i) != '0'){
+                throw new IllegalArgumentException("Illegal value");
+            }
+        }
+
+       if (value.length() > 8){
+           throw new StringIndexOutOfBoundsException("invalid value");
+       }
+    }
+    public void addToIncompleteBit(String value){
+
+       for (int i = 0; i < value.length() ; i++) {
+               if (value.length() < 8){
+                   value = '0' + value;
+           }
+       }
+
+    }
+    public void powerBit(String value){
+        validateValue(value);
+       if (value.charAt(7) == '0') value = "0";
+
+        }
+
+
     public void display(){
         for (int[] row : myArray) {
             for (int column : row) {
