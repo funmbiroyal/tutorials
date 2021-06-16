6b502c1b8c541fbddc2e99298b226b2feb2d645e
Grace <funmibiajadi@gmail.com>
2021-06-16 14:49:38 +0100

    Triangle unit Test

--- test/Chapter8Test/TriangleTest.java
+++ test/Chapter8Test/TriangleTest.java
@@ -1,2 +1,37 @@
-package Chapter8Test;public class TriangleTest {
+package Chapter8Test;
+
+import Chapter8.Triangle;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TriangleTest {
+    Triangle triangle;
+    @BeforeEach
+    void startEachTestWith(){
+        triangle = new Triangle();
+    }
+    @Test
+    void canCreateTriangleTest(){
+        Triangle triangle = new Triangle();
+        assertNotNull(triangle);
+    }
+    @Test
+    void canCalculatePerimeterOfTriangleTest(){
+        triangle.setLength(5);
+        assertEquals(5,triangle.getLength());
+        triangle.setWidth(10);
+        assertEquals(10,triangle.getWidth());
+        assertEquals(30,triangle.calculatePerimeter(5,10));
+    }
+    @Test
+    void canCalculateAreaOfTriangleTest(){
+        triangle.setLength(5);
+        assertEquals(5,triangle.getLength());
+        triangle.setWidth(10);
+        assertEquals(10,triangle.getWidth());
+        assertEquals(50, triangle.calculateArea());
+    }
 }
