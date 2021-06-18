36f568e03405826c7dcaf2f9ea3913aee19d5f23
Grace <funmibiajadi@gmail.com>
2021-06-18 18:54:32 +0100

    Modified

--- src/Chapter4/Factorial.java
+++ src/Chapter4/Factorial.java
@@ -1,7 +1,7 @@
 package Chapter4;
 
 public class Factorial {
-    public static int factorialOfNumber(int number){
+    public static int findFactorialOfNumber(int number){
         int factorial = 1;
         if (number == 0){
             return 1;
@@ -9,6 +9,7 @@ public class Factorial {
         else if (number == 1){
             return 1;
         }else {
+
            while (number >=2 ){
                 factorial  *= number;
                 number--;
@@ -18,8 +19,25 @@ public class Factorial {
         }
         return factorial;
     }
+    public static double findMathConstant(int value){
+        double e;
+        double factorial = 1;
+        if (value == 0){
+            return  e = 1 + 1.0;
+        }else if (value == 1){
+            e = 1 + 1.0;
+        }else {
+            while (value >= 2){
+                factorial += (1.0/findFactorialOfNumber(value));
+                value--;
+
+            }
+        }
+        return e = 1 + factorial;
+    }
 
     public static void main(String[] args) {
-        System.out.println(factorialOfNumber(5));
+        System.out.println(findFactorialOfNumber(5));
+        System.out.println(findMathConstant(30));
     }
 }
