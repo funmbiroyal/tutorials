22f30adb726b6c7dc214139a34d335cca9b4e055
Grace <funmibiajadi@gmail.com>
2021-06-19 23:33:39 +0100

    Movie flight === unit test

--- test/Chapter7Test/MovieFlightTest.java
+++ test/Chapter7Test/MovieFlightTest.java
@@ -1,2 +1,20 @@
-package Chapter7Test;public class MovieFlightTest {
+package Chapter7Test;
+
+import Chapter7.MovieFlight;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MovieFlightTest {
+    @Test
+    void MovieFlightCanBeCreated(){
+        MovieFlight movieFlight = new MovieFlight();
+        assertNotNull(movieFlight);
+    }
+    @Test
+    void userCanWatchTwoDifferentMovies(){
+        int [] moviesDurations = {30,40,60,10,50,70,50,80,20,90,100};
+        assertTrue(MovieFlight.isMoviesDurationEqualToFlightDuration(moviesDurations,100));
+    }
 }
