05cfe3b7b6900b7baa62490356e7b904b88c7a60
Grace <funmibiajadi@gmail.com>
2021-06-24 17:17:42 +0100

    pattern Matching Regular Expressions

--- src/Chapter14/PatternMatching.java
+++ src/Chapter14/PatternMatching.java
@@ -1,2 +1,18 @@
-package Chapter14;public class PatternMatching {
+package Chapter14;
+
+import java.util.regex.Pattern;
+
+public class PatternMatching {
+    public static void main(String[] args) {
+
+        String text    =
+                "is the text to be searched " +
+                        "for occurrences of the pattern.";
+
+        String pattern = ".*is.*";
+
+        boolean matches = Pattern.matches(pattern,text);
+
+        System.out.println("matches = " + matches);
+    }
 }
