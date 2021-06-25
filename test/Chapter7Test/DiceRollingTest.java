b95bb09bb8f1ab63ea1a21e3362cb15643ad9eef
Grace <funmibiajadi@gmail.com>
2021-06-26 00:01:12 +0100

    Dice Rolling Unit test

--- test/Chapter7Test/DiceRollingTest.java
+++ test/Chapter7Test/DiceRollingTest.java
@@ -1,2 +1,43 @@
-package Chapter7Test;public class DiceRollingTest {
+package Chapter7Test;
+
+import Chapter7.DiceRolling;
+import org.junit.jupiter.api.Test;
+
+import java.security.SecureRandom;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DiceRollingTest {
+    @Test
+    void DiceRollingCanBeCreatedTest(){
+        DiceRolling diceRolling = new DiceRolling();
+        assertNotNull(diceRolling);
+
+    }
+    @Test
+    void CanGenerateNumberRandomly(){
+        SecureRandom generator = new SecureRandom();
+        int generatedNumber = 1 + generator.nextInt(6);
+        int generatedNumber2 = 1 + generator.nextInt(6);
+        assertNotEquals(generatedNumber,generatedNumber2);
+    }
+    @Test
+    void canCalculateTwoGeneratedRandomNumbers(){
+        SecureRandom generator = new SecureRandom();
+        int generatedNumber = 1 + generator.nextInt(6);
+        int generatedNumber2 = 1 + generator.nextInt(6);
+        int result = generatedNumber + generatedNumber2;
+        System.out.println(generatedNumber);
+        System.out.println(generatedNumber2);
+        System.out.println(result);
+
+        assertEquals(result ,generatedNumber + generatedNumber2);
+    }
+
+    @Test
+    void canRollDie(){
+        DiceRolling.rollDie();
+        DiceRolling.calculateSumOfDie();
+    }
 }
