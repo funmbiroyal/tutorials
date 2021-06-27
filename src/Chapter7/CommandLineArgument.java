d0f0a10d0688bfdd42976d7f96602bd10f6752ca
Grace <funmibiajadi@gmail.com>
2021-06-27 23:44:06 +0100

    Not through

--- src/Chapter7/CommandLineArgument.java
+++ src/Chapter7/CommandLineArgument.java
@@ -1,2 +1,16 @@
-package Chapter7;public class CommandLineArgument {
+package Chapter7;
+
+public class CommandLineArgument {
+//    public static String commandLine(String [] args){
+//
+//
+//    }
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+
+            System.out.println(args[i]);
+
+        }
+    }
+
 }
