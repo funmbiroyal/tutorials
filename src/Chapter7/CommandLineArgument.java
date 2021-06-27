25f8d28920124415aa5306ede504b89315b72062
Grace <funmibiajadi@gmail.com>
2021-06-28 00:27:32 +0100

    through ----- command line Argument

--- src/Chapter7/CommandLineArgument.java
+++ src/Chapter7/CommandLineArgument.java
@@ -1,16 +1,31 @@
 package Chapter7;
 
 public class CommandLineArgument {
-//    public static String commandLine(String [] args){
-//
-//
-//    }
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
+        if (args.length != 3) {
+            System.out.println("Error: Please re-enter the entire command, " +
+                    "including an array size, initial value and increment");
+        } else {
+            int lengthOfArray = Integer.parseInt(args[0]);
+            int initialValue = Integer.parseInt(args[1]);
+            int increment = Integer.parseInt(args[2]);
+            int[] array = new int[lengthOfArray];
 
-            System.out.println(args[i]);
+
+
+            for (int counter = initialValue; counter < array.length; counter++)
+                array[counter] = initialValue = initialValue + increment;
+
+                System.out.printf("%s%8s%n", "Index", "Value");
+
+
+
+            for (int counter = 0; counter < array.length; counter++) {
+                System.out.printf("%5d%8d%n", counter, array[counter]);
+
+            }
 
         }
-    }
 
+    }
 }
