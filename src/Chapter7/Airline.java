b0feb54eaa684035ed461da3686ddd58bfdd495e
Grace <funmibiajadi@gmail.com>
2021-07-06 11:31:53 +0100

    Airline

--- src/Chapter7/Airline.java
+++ src/Chapter7/Airline.java
@@ -4,11 +4,7 @@ public class Airline {
     private boolean[] firstClass;
     private boolean[] economy;
     private boolean[] economyClassArrangement;
-    private static int numberOfSeat;
-
-
     private boolean[] firstClassArrangement;
-    private boolean hasBooked = false;
 
     public Airline() {
         economy = new boolean[]{false, false, false, false, false};
@@ -25,11 +21,7 @@ public class Airline {
         return false;
     }
 
-    public void bookSeat(int numberOfSeat) throws CannotBookASeatTwiceException, SeatAlreadyOccupiedException {
-        validateEconomySeat(numberOfSeat);
-        Airline.numberOfSeat = numberOfSeat;
 
-    }
 
     public void setEconomyClassArrangement(boolean[] arrangement) {
 
@@ -51,45 +43,8 @@ public class Airline {
         return firstClassArrangement;
     }
 
-    private void validateEconomySeat(int seatNumber) throws CannotBookASeatTwiceException, SeatAlreadyOccupiedException {
-        if (seatNumber == numberOfSeat) {
-           throw new CannotBookASeatTwiceException("You Cannot book more than a seat!");
-        } else if (seatNumber > 1) {
-            throw new SeatAlreadyOccupiedException("Sorry,seat has been occupied!");
-        } else {
-            System.out.println("You have successfully booked a seat!");
-        }
 
-    }
-
-    public void changeToFirstClass(int numberOfSeat, boolean hasBooked) {
-        validateFirstClassSeat(hasBooked);
-        Airline.numberOfSeat = numberOfSeat;
-
-    }
-
-    private void validateFirstClassSeat(boolean hasBooked) {
-        for (boolean seat : firstClass) {
-            if (!seat) {
-                firstClass[0] = hasBooked;
-                System.out.println("Thanks for changing to first class,you have booked a seat!");
-                break;
-            }
-        }
-
-    }
-
-    public boolean wantFirstClass(boolean response) {
-
-        if (response) {
-            changeToFirstClass(numberOfSeat, hasBooked);
-        }else {
-            System.out.println("Next flight leaves in 3 hours.....");
-        }
-          return true;
-    }
 }
-
 
 
 
