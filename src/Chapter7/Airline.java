9d09a03f0507b3677aad939a375441c5a4e3adb7
Grace <funmibiajadi@gmail.com>
2021-07-13 11:32:55 +0100

    Refactored Airline class

--- src/Chapter7/Airline.java
+++ src/Chapter7/Airline.java
@@ -3,54 +3,36 @@ package Chapter7;
 public class Airline {
     private boolean[] firstClass;
     private boolean[] economy;
-    private boolean[] economyClassArrangement;
-    private boolean[] firstClassArrangement;
+    private int firstClassSeatCounter;
+    private int economyClassSeatCounter = 5;
 
     public Airline() {
         economy = new boolean[]{false, false, false, false, false};
         firstClass = new boolean[]{false, false, false, false, false};
     }
 
-    public boolean getFirstClassSeat() {
+    public int assignSeat(boolean isFirstClass) {
+        int seatNumber;
+        if (isFirstClass) {
+            firstClass[firstClassSeatCounter] = true;
+            seatNumber = ++firstClassSeatCounter;
+        }else if(firstClass[0] && firstClass[1] && firstClass[2] && firstClass[3] && firstClass[4]){
+            economy[economyClassSeatCounter - 5] = true;
+            seatNumber = ++economyClassSeatCounter;
 
-        return false;
+      }else if(economy[0] && economy[1] && economy[2] && economy[3] && economy[4]){
+        firstClass[firstClassSeatCounter] = true;
+        seatNumber = ++firstClassSeatCounter;
+
+
+        }else {
+            economy[economyClassSeatCounter - 5] = true;
+            seatNumber = ++economyClassSeatCounter;
+        }
+        return seatNumber;
     }
-
-    public boolean getEconomyClassSeat() {
-
-        return false;
-    }
-
-
-
-    public void setEconomyClassArrangement(boolean[] arrangement) {
-
-        economyClassArrangement = arrangement;
-    }
-
-    public boolean[] getEconomyClassArrangement() {
-
-        return economyClassArrangement;
-    }
-
-    public void setFirstClassArrangement(boolean[] arrangement) {
-
-        firstClassArrangement = arrangement;
-    }
-
-    public boolean[] getFirstClassArrangement() {
-
-        return firstClassArrangement;
-    }
-
 
 }
-
-
-
-
-
-
 
 
 
