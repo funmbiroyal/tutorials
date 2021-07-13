d77c0df876bde9801c1b84210be328a626441f78
Grace <funmibiajadi@gmail.com>
2021-07-13 11:34:30 +0100

    Refactored Airlin Unit Test

--- test/Chapter7Test/AirlineTest.java
+++ test/Chapter7Test/AirlineTest.java
@@ -2,12 +2,10 @@ package Chapter7Test;
 
 import Chapter7.Airline;
 import Chapter7.Passenger;
-import Chapter7.CannotBookTwoSeatException;
+import Chapter7.FirstClassIsFullException;
 import Chapter7.SeatAlreadyOccupiedException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertThat;
@@ -16,93 +14,82 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AirlineTest {
     Airline airline;
     Passenger passenger;
+    Passenger grace;
+    Passenger pass;
+    Passenger bessie;
+    Passenger joy;
+    Passenger fineBoy;
+
     @BeforeEach
-    void startEachTestWith(){
+    void startEachTestWith() {
         airline = new Airline();
-        passenger = new Passenger(012,"lilly","smart");
+        passenger = new Passenger(01, "lilly", "smart");
+        grace = new Passenger(02, "lilly", "smart");
+        pass = new Passenger(03, "lilly", "smart");
+        bessie = new Passenger(04, "lilly", "smart");
+        joy = new Passenger(05, "lilly", "smart");
+        fineBoy = new Passenger(06, "lilly", "smart");
     }
+
     @Test
-    void airlineCanBeCreatedTest(){
+    void airlineCanBeCreatedTest() {
         Airline airline = new Airline();
         assertNotNull(airline);
 
     }
+
     @Test
-    void passengerCanBeCreatedTest(){
-       Passenger passenger = new Passenger(012,"lilly","smart");
-       assertNotNull(passenger);
-    }
-    @Test
-    void seatIsAvailableInFirstClassTest(){
-        boolean[] seat = new boolean[5];
-        Arrays.fill(seat,false);
-        assertFalse(airline.getFirstClassSeat());
-    }
-    @Test
-    void seatIsAvailableInEconomyClassTest(){
-        boolean[] seat = new boolean[5];
-        Arrays.fill(seat,false);
-        assertFalse(airline.getEconomyClassSeat());
-    }
-    @Test
-    void passengerCanBookSeatInEconomyClassTest() throws CannotBookTwoSeatException, SeatAlreadyOccupiedException {
-        boolean[] seat = new boolean[5];
-        Arrays.fill(seat,false);
-        assertFalse(airline.getFirstClassSeat());
-        passenger.bookSeat(1);
-        boolean [] newArrangement ={true,false,false,false,false};
-        airline.setEconomyClassArrangement(newArrangement);
-        assertEquals(newArrangement,airline.getEconomyClassArrangement());
-        System.out.println(Arrays.toString(newArrangement));
-    }
-    @Test
-    void passengerCannotBookASeatTwiceInEconomyClassTest() throws CannotBookTwoSeatException, SeatAlreadyOccupiedException {
-        boolean[] seat = new boolean[5];
-        Arrays.fill(seat,false);
-        assertFalse(airline.getFirstClassSeat());
-        passenger.bookSeat(1);
-        boolean [] newArrangement ={true,false,false,false,false};
-        airline.setEconomyClassArrangement(newArrangement);
-        assertEquals(newArrangement,airline.getEconomyClassArrangement());
-        assertThrows(SeatAlreadyOccupiedException.class,()->passenger.bookSeat(1));
-        assertEquals(newArrangement,airline.getEconomyClassArrangement());
-        System.out.println(Arrays.toString(newArrangement));
-    }
-    @Test
-    void passengerCannotBookTwoSeatInEconomyClassTest() throws CannotBookTwoSeatException, SeatAlreadyOccupiedException {
-        boolean[] seat = new boolean[5];
-        Arrays.fill(seat,false);
-        assertFalse(airline.getFirstClassSeat());
-        boolean [] newArrangement ={true,false,false,false,false};
-        airline.setEconomyClassArrangement(newArrangement);
-        assertEquals(newArrangement,airline.getEconomyClassArrangement());
-        passenger.bookSeat(1);
-        assertThrows(CannotBookTwoSeatException.class,()-> passenger.bookSeat(2));
-        assertEquals(newArrangement,airline.getEconomyClassArrangement());
-        System.out.println(Arrays.toString(newArrangement));
-    }
-    @Test
-    void passengerCannotBookSeatInEconomyClassWhenFull_butChangeToFirstClassTest(){
-        boolean [] seat = new boolean[5];
-        Arrays.fill(seat,true);
-        boolean [] newArrangementOfEconomyClass = {true,true,true,true,true};
-        airline.setEconomyClassArrangement(newArrangementOfEconomyClass);
-        assertEquals(newArrangementOfEconomyClass,airline.getEconomyClassArrangement());
-        passenger.wantFirstClass(true);
-        boolean [] newArrangementOfFirstClass ={false,false,false,false,true};
-        airline.setFirstClassArrangement(newArrangementOfFirstClass);
-        assertEquals(newArrangementOfFirstClass,airline.getFirstClassArrangement());
-    }
-    @Test
-    void throwsAllExceptions() throws CannotBookTwoSeatException, SeatAlreadyOccupiedException {
-        boolean [] seat = new boolean[5];
-        assertFalse(airline.getFirstClassSeat());
-        passenger.bookSeat(1);
-        assertThrows(SeatAlreadyOccupiedException.class,()->passenger.bookSeat(1));
-        assertThrows(CannotBookTwoSeatException.class,()->passenger.bookSeat(5));
-        boolean [] newArrangement ={true,false,false,false,false};
-        airline.setEconomyClassArrangement(newArrangement);
-        assertEquals(newArrangement,airline.getEconomyClassArrangement());
+    void passengerCanBeCreatedTest() {
+        Passenger passenger = new Passenger(012, "lilly", "smart");
+        assertNotNull(passenger);
     }
 
+    @Test
+    void passengerCanBookSeatTest() throws FirstClassIsFullException, SeatAlreadyOccupiedException {
+        passenger.bookSeat(true,airline);
+        assertEquals(1, passenger.getSeatNumber());
+
+        grace.bookSeat(true,airline);
+        assertEquals(2,grace.getSeatNumber());
+
+        pass.bookSeat(true,airline);
+        assertEquals(3, pass.getSeatNumber());
+
+    }
+    @Test
+    void passengerCanChooseOtherClassIfOneIsFull() throws ArrayIndexOutOfBoundsException {
+        passenger.bookSeat(true,airline);
+        assertEquals(1, passenger.getSeatNumber());
+
+        grace.bookSeat(true,airline);
+        assertEquals(2,grace.getSeatNumber());
+
+        pass.bookSeat(true,airline);
+        assertEquals(3, pass.getSeatNumber());
+
+        bessie.bookSeat(true,airline);
+        assertEquals(4,bessie.getSeatNumber());
+
+        joy.bookSeat(true,airline);
+        assertEquals(5,joy.getSeatNumber());
+
+
+        assertThrows(ArrayIndexOutOfBoundsException.class,()-> fineBoy.bookSeat(true, airline));
+
+        fineBoy.bookSeat(false,airline);
+        assertEquals(6,fineBoy.getSeatNumber());
+
+        grace.bookSeat(false,airline);
+        assertEquals(7,grace.getSeatNumber());
+
+        pass.bookSeat(false,airline);
+        assertEquals(8, pass.getSeatNumber());
+
+        bessie.bookSeat(false,airline);
+        assertEquals(9,bessie.getSeatNumber());
+
+        passenger.bookSeat(false,airline);
+        assertEquals(10, passenger.getSeatNumber());
+
+    }
 }
