ef52c8d2d25c9d0319c1e65687e159d0b419d52d
Grace <funmibiajadi@gmail.com>
2021-07-14 22:43:01 +0100

    Main method airline

--- src/Chapter7/AirlineMainMethod.java
+++ src/Chapter7/AirlineMainMethod.java
@@ -1,2 +1,33 @@
-package Chapter7;public class AirlineMainMethod {
+package Chapter7;
+
+import java.util.Scanner;
+
+public class AirlineMainMethod {
+    public static void main(String[] args) {
+        Airline airline = new Airline();
+        Passenger passenger = new Passenger(012,"lilly","smart");
+        Scanner scanner = new Scanner(System.in);
+        int userChoice;
+        do {
+            String prompt = """
+                Hi,Welcome to Java Airline!...
+                ------------------------------
+                for Economy  Class, press 1.
+                for First Class, press 2.   
+                """;
+            System.out.println(prompt);
+            userChoice = scanner.nextInt();
+            if (userChoice == 1){
+                passenger.bookSeat(false,airline);
+            }else if (userChoice == 2){
+                passenger.bookSeat(true,airline);
+            }else{
+                System.out.println("Invalid Input!");
+            }
+
+
+        } while (userChoice != 0);
+
+
+    }
 }
--- src/Chapter7/SeatAlreadyOccupiedException.java
+++
@@ -1,6 +0,0 @@
-package Chapter7;
-
-public class SeatAlreadyOccupiedException extends Throwable {
-    public SeatAlreadyOccupiedException(String s) {
-    }
-}
