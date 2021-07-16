d0eb05d3d0fbfbc638e63d2ca2076e61a72a4775
Grace <funmibiajadi@gmail.com>
2021-07-16 18:38:09 +0100

    Arrays to List

--- src/Chapter16/ListToArray.java
+++ src/Chapter16/ListToArray.java
@@ -1,2 +1,24 @@
-package Chapter16;public class ListToArray {
+
+package Chapter16;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListToArray {
+    public static void main(String[] args) {
+        String [] colors = {"Black","Blue","Red"};
+        LinkedList<String> linkedList = new LinkedList<>(Arrays.asList(colors));
+        linkedList.addLast("Yellow");
+        linkedList.add("Orange");
+        linkedList.add(4,"purple");
+        linkedList.addFirst("Cyan");
+
+         colors = linkedList.toArray(new String[linkedList.size()]);
+
+        System.out.println("Colors: ");
+        for (String color : colors ){
+          System.out.println(color);
+      }
+    }
 }
