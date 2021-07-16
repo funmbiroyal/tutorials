88eab4b4424c4db3ff9bb4db2d11ef9e12d145fd
Grace <funmibiajadi@gmail.com>
2021-07-16 18:34:31 +0100

    Set Collection

--- src/Chapter16/Sets.java
+++ src/Chapter16/Sets.java
@@ -1,2 +1,25 @@
-package Chapter16;public class Sets {
+package Chapter16;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Sets {
+    public static  void printNoDuplicate(List<String>values){
+        Set<String>set = new HashSet<>(values);
+        System.out.printf("%nNon duplicates are: ");
+        for (String value: set ){
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        String [] colors = {"red", "white", "blue", "green", "gray",
+                "orange", "tan", "white", "cyan", "peach", "gray", "orange"};
+        List<String> myList = Arrays.asList(colors);
+        System.out.printf("List: %s%n", myList);
+        printNoDuplicate(myList);
+    }
 }
