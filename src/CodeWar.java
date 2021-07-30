5fab10582e65a803e76588476e92ca8d1ca85e54
Grace <funmibiajadi@gmail.com>
2021-07-30 18:27:40 +0100

    Code war2

--- src/CodeWar.java
+++ src/CodeWar.java
@@ -1,25 +1,36 @@
 import org.junit.jupiter.api.Test;
 
-public class CodeWar {
-    public static void function(int [] numbers, int value){
+import java.util.Arrays;
 
-        for (int i = 0; i < numbers.length ; i++) {
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+
+public class CodeWar {
+    public static int [] function(int[] numbers, int value) {
+        int [] result = new int[2];
+
+        for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers.length; j++) {
-                if (value == numbers[i] + numbers[j]){
-                    System.out.print(i + " ");
-                    break;
+                if (value == numbers[i] + numbers[j]) {
+                   result[0]= j;
+                   result[1] = i;
                 }
             }
 
         }
+        return result;
     }
 
     public static void main(String[] args) {
-        int [] array = {1,2,3,4,5};
+        int[] array = {1, 2, 3, 4, 5};
         int value = 3;
-        function(array,value);
+        System.out.println(Arrays.toString(function(array, value)));
     }
 
-//
+    @Test
+    void testFunction() {
+        int[] array = {1, 2, 3, 4, 5};
+        int value = 3;
+        assertNotSame(array, value);
     }
 }
+
