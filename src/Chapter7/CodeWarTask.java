35f557074e9ca2b238fe00e64a8f0ee29dda2ee5
Grace <funmibiajadi@gmail.com>
2021-07-31 22:13:12 +0100

    RE-d0

--- src/Chapter7/CodeWarTask.java
+++ src/Chapter7/CodeWarTask.java
@@ -1,2 +1,24 @@
-package Chapter7;public class CodeWarTask {
+package Chapter7;
+
+import java.util.Arrays;
+
+public class CodeWarTask {
+    public static int [] findIndicesOfTwoAddedNumber(int [] numbers,int value) {
+        int [] newArray = new int[2];
+        for(int i = 0; i <numbers.length; i++) {
+            for(int j = 0; j <numbers.length; j++) {
+                if (value == numbers[i] + numbers[j]){
+                    newArray[0] = i;
+                    newArray[1] = j;
+                }
+            }
+        }
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+        int numbers [] ={1,2,3,5};
+        int value = 3;
+        System.out.println(Arrays.toString(findIndicesOfTwoAddedNumber(numbers, value)));
+    }
 }
