eb327e8bbd648b4001bc4a3cbd7590bed18416ee
Grace <funmibiajadi@gmail.com>
2021-08-29 21:27:52 +0100

    Pen class of turtle

--- src/Chapter7/myTurtle/entities/Pen.java
+++ src/Chapter7/myTurtle/entities/Pen.java
@@ -1,2 +1,16 @@
-package Chapter7.myTurtle.entities;public class Pen {
+package Chapter7.myTurtle.entities;
+
+public class Pen {
+    private boolean penPosition;
+
+    public void setPenUp(boolean isUp){
+        penPosition = isUp;
+    }
+ public boolean isUp(){
+        return penPosition;
+    }
+    public void setPenDown(boolean isDown){
+        penPosition = isDown;
+    }
+
 }
