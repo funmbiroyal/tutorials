59765be0a2af47ac536227483dfb2a9d0d073fd4
Grace <funmibiajadi@gmail.com>
2021-09-03 22:03:17 +0100

    Towers of hanoi solved with recursion

--- src/Chapter18/TowersOfHanoi.java
+++ src/Chapter18/TowersOfHanoi.java
@@ -1,2 +1,22 @@
-package Chapter18;public class TowersOfHanoi {
+package Chapter18;
+
+public class TowersOfHanoi {
+    public static void solveTowers(int disks, int sourcePeg,
+                                   int destinationPeg, int tempPeg) {
+        if (disks == 1) {
+            System.out.printf("%n%d --> %d", sourcePeg, destinationPeg);
+            return;
+        }
+        solveTowers(disks - 1, sourcePeg, tempPeg, destinationPeg);
+        System.out.printf("%n%d --> %d", sourcePeg, destinationPeg);
+        solveTowers(disks - 1, tempPeg, destinationPeg, sourcePeg);
+    }
+
+    public static void main(String[] args) {
+        int startPeg = 1;
+        int endPeg = 3;
+        int tempPeg = 2;
+        int totalDisks = 3;
+        solveTowers(totalDisks, startPeg, endPeg, tempPeg);
+    }
 }
