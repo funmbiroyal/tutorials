81bb7f00623b06ba51f98b96a3178f3ce97eef67
Grace <funmibiajadi@gmail.com>
2021-09-29 23:18:28 +0100

    Hare class, new updates

--- src/Chapter7/tortoise/Hare.java
+++ src/Chapter7/tortoise/Hare.java
@@ -1,2 +1,36 @@
-package Chapter7.tortoise;public class Hare {
-}
+package Chapter7.tortoise;
+
+public class Hare extends Player{
+    public int bigHop(){
+        int position = Hare.getPosition();
+        position+=9;
+        return position;
+    }
+    public int smallHop(){
+        int position = Hare.getPosition();
+        position+=1;
+        return position;
+    }
+    public int bigSlip(){
+        int position = 0;
+       if (Hare.getPosition() >12){
+           position -=12;
+       }else{
+           position =1;
+       }
+       return position;
+       }
+
+    public int smallSlip(){
+        int position = 0;
+        if (Hare.getPosition() >2){
+            position -=2;
+        }else{
+            position =1;
+        }
+        return position;
+    }
+    public void fallAsSleep(){
+       int position = Hare.getPosition();
+    }
+    }
