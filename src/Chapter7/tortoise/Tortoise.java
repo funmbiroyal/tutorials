6f855a38a3f06af1c63b2aab9b141674b07cc01f
Grace <funmibiajadi@gmail.com>
2021-09-29 23:17:51 +0100

    Tortoise class ,new update

--- src/Chapter7/tortoise/Tortoise.java
+++ src/Chapter7/tortoise/Tortoise.java
@@ -1,22 +1,27 @@
 package Chapter7.tortoise;
 
 public class Tortoise extends Player{
-    private int position;
 
-    public void fastPlod(){
-        position += 3;
+    public static int fastPlod(){
+      int position = Tortoise.getPosition();
+      position += 3;
+      return position;
     }
 
-    public void slowPod(){
+    public static int  slowPod(){
+        int position = Tortoise.getPosition();
         position += 1;
-    }
-    public void slip(){
-        if(position > 6){
-            position -= 6;
-        }else{
-            position = 1;
-        }
+        return position;
 
+    }
+    public int slip(){
+        int position = 0;
+        if (Hare.getPosition() > 6){
+            position -=6;
+        }else{
+            position =1;
+        }
+        return position;
     }
 
 }
