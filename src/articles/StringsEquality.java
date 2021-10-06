c09eef47e90485d0ee82fdeb1a99bd6f7b66a524
Grace <funmibiajadi@gmail.com>
2021-10-06 15:04:51 +0100

    String equality check

--- src/articles/StringsEquality.java
+++ src/articles/StringsEquality.java
@@ -1,2 +1,25 @@
-package articles;public class StringsEquality {
+package articles;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class StringsEquality {
+
+    public boolean checkEquality(String[] array1, String[] array2) {
+        if (Arrays.equals(array1, array2)){
+            return true;
+        }
+        if (array1 == null || array2==null){
+            return false;
+        }
+        if (array1.length != array2.length){
+            return false;
+        }
+        for (int i = 0; i < array1.length ; i++) {
+            if (!array1[i].equals(array1[i])){
+                return false;
+            }
+        }
+        return true;
+    }
 }
