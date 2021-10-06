a97814ea1c038ffad61dddf5b589bcf3094acfa6
Grace <funmibiajadi@gmail.com>
2021-10-06 15:03:24 +0100

    Main method

--- src/articles/StringEqualityMain.java
+++ src/articles/StringEqualityMain.java
@@ -1,2 +1,11 @@
-package articles;public class StringEqualityMain {
+package articles;
+
+public class StringEqualityMain {
+    public static void main(String[] args) {
+        StringsEquality equality = new StringsEquality();
+        String [] array1 = {"A","B","C" };
+        String [] array2 = {"A","B","C" };
+        boolean isEqual = equality.checkEquality(array1,array2);
+        System.out.println(isEqual);
+    }
 }
