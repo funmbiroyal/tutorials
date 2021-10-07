c01f586f1fc76e5d358f31fcfde29155b676b86e
Grace <funmibiajadi@gmail.com>
2021-10-07 05:13:02 +0100

    Searching for node in a binary tree, unit test

--- src/somWork/BinaryNodeTreeTest.java
+++ src/somWork/BinaryNodeTreeTest.java
@@ -1,19 +1,24 @@
-package articles;
+package somWork;
 
 import org.junit.jupiter.api.Test;
 
-public class BinaryNodeTreeTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+public class BinaryNodeTreeTest {
+public  BinaryNodeTree.Node root;
     @Test
     void canFindNodeFromABinaryTree_ifPresentReturnTrue_elseReturnFalse(){
         int key = 4;
-        BinaryNodeTree.Node root = new BinaryNodeTree.Node(0);
+        int key2 = 14;
+        root = new BinaryNodeTree.Node(0);
         root.left = new BinaryNodeTree.Node(1);
         root.right = new BinaryNodeTree.Node(2);
         root.left.left = new BinaryNodeTree.Node(3);
-        root.right.right = new BinaryNodeTree.Node(4);
-        
+        root.left.right = new BinaryNodeTree.Node(4);
+        root.right.left = new BinaryNodeTree.Node(5);
+        root.right.right = new BinaryNodeTree.Node(6);
 
-        BinaryNodeTree.nodeExists();
+        assertTrue(BinaryNodeTree.nodeExists(root,key));
+        assertFalse(BinaryNodeTree.nodeExists(root,key2));
     }
 }
