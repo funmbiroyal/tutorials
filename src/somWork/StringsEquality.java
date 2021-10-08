a8308ca61c42e86298342733e0f9c9c53555cf02
Grace <funmibiajadi@gmail.com>
2021-10-08 13:02:32 +0100

    String equality

--- src/somWork/StringsEquality.java
+++ src/somWork/StringsEquality.java
@@ -1,25 +1,28 @@
-package articles;
+package somWork;
 
-import java.util.Arrays;
-import java.util.Objects;
 
 public class StringsEquality {
 
-    public boolean checkEquality(String[] array1, String[] array2) {
-        if (Arrays.equals(array1, array2)){
+    public boolean checkEquality(String[] string1, String[] string2) {
+        if (string1 == string2) {
+
             return true;
         }
-        if (array1 == null || array2==null){
+        if(string1 == null || string2 == null) {
+
             return false;
         }
-        if (array1.length != array2.length){
-            return false;
-        }
-        for (int i = 0; i < array1.length ; i++) {
-            if (!array1[i].equals(array1[i])){
-                return false;
+        if (string1.length == string2.length) {
+            for (int i = 0; i < string1.length; i++) {
+                if (!string1[i].equals(string2[i])){
+
+                    return false;
+                }
             }
+            return true;
         }
-        return true;
+
+        return false;
+        }
     }
-}
+
