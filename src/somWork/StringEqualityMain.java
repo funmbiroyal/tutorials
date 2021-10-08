4fec93022d77be17cecf69e4578df5977286c305
Grace <funmibiajadi@gmail.com>
2021-10-08 12:51:18 +0100

    check if two strings array are equal, main method

--- src/somWork/StringEqualityMain.java
+++ src/somWork/StringEqualityMain.java
@@ -1,11 +1,16 @@
-package articles;
+package somWork;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+//fixme
 public class StringEqualityMain {
     public static void main(String[] args) {
         StringsEquality equality = new StringsEquality();
-        String [] array1 = {"A","B","C" };
-        String [] array2 = {"A","B","C" };
+        String [] array1 = {"Ade","Boy","Cold" };
+        String [] array2 = {"Ade","Boy","Cold" };
         boolean isEqual = equality.checkEquality(array1,array2);
         System.out.println(isEqual);
+
     }
 }
