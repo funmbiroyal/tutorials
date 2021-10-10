01b52e6ace4003f4a8d49f70c42932b563e7f8a3
Grace <funmibiajadi@gmail.com>
2021-10-10 23:11:59 +0100

    abstract class player

--- src/Chapter7/tortoise/Player.java
+++ src/Chapter7/tortoise/Player.java
@@ -1,2 +1,16 @@
-package Chapter7.tortoise;public class Player {
+package Chapter7.tortoise;
+
+public abstract class Player {
+    private static int position;
+
+    public static int getPosition() {
+        return position;
+    }
+
+    public static void setPosition(int position) {
+        Player.position = position;
+    }
+
+
+
 }
