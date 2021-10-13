83b9786272ae66de03bad4f94ed78b40851fd233
Grace <funmibiajadi@gmail.com>
2021-10-13 09:15:10 +0100

    main

---
+++ src/somWork/AdjacentDuplicateMain.java
@@ -0,0 +1,9 @@
+package somWork;
+
+public class AdjacentDuplicateMain {
+    public static void main(String[] args) {
+        AdjacentDuplicate adjacentDuplicate = new AdjacentDuplicate();
+        System.out.println( adjacentDuplicate.removeDuplicates("aabaarbarccrabmq"));
+    }
+
+}
