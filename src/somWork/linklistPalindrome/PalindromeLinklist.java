c3ed9ea8b525886b976b239b317ad6fab7bf9969
Grace <funmibiajadi@gmail.com>
2021-10-17 22:58:16 +0100

    Check if link list is a palindrome

--- src/somWork/linklistPalindrome/PalindromeLinklist.java
+++ src/somWork/linklistPalindrome/PalindromeLinklist.java
@@ -1,38 +1,36 @@
-package somWork;
+package somWork.linklistPalindrome;
 
 import java.util.Stack;
 
 public class PalindromeLinklist {
-   
-    static boolean isPalindrome(Node head)
-    {
-        // This pointer will allow the first traversal
-        // of the linked list
-        Node next = head;
+        static boolean isPalindrome(Node head) {
+            // This pointer will allow the first traversal
+            // of the linked list
+            Node next = head;
 
-        boolean ispalin = true;
-        Stack<Integer> stack = new Stack<Integer>();
+            boolean ispalin = true;
+            Stack<Integer> stack = new Stack<Integer>();
 
-        // Traverse the linked list and add its elements
-        // to the stack
-        while (next != null) {
-            stack.push(next.data);
-            next = next.ptr;
-        }
-        // Iterate the linked list again and
-        // check by each element with the stack
-        while (head != null) {
-            int i = stack.pop();
-            if (head.data == i) {
-                ispalin = true;
+            // Traverse the linked list and add its elements
+            // to the stack
+            while (next != null) {
+                stack.push(next.data);
+                next = next.ptr;
             }
-            else {
-                ispalin = false;
-                break;
+            // Iterate the linked list again and
+            // check by each element with the stack
+            while (head != null) {
+                int i = stack.pop();
+                if (head.data == i) {
+                    ispalin = true;
+                } else {
+                    ispalin = false;
+                    break;
+                }
+                // Move to the next element in stack and the list
+                head = head.ptr;
             }
-            // Move to the next element in stack and the list
-            head = head.ptr;
+            return ispalin;
         }
-        return ispalin;
-    }
+
 }
