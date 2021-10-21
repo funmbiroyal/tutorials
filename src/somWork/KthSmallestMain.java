b99c92f16793080a9d972eaac9041d9ad18d4d33
Grace <funmibiajadi@gmail.com>
2021-10-21 05:01:01 +0100

    Kth smallest main method

--- src/somWork/KthSmallestMain.java
+++ src/somWork/KthSmallestMain.java
@@ -1,2 +1,26 @@
-package somWork;public class KthSmallestMain {
+package somWork;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static somWork.KthSmallest.kthSmallest;
+
+public class KthSmallestMain {
+    public static void main(String[] args) {
+            ArrayList<ArrayList<Integer>> mat = new ArrayList<ArrayList<Integer>>();
+            mat.add(new ArrayList<Integer>(Arrays.asList(11, 21, 31, 41)));
+            mat.add(new ArrayList<Integer>(Arrays.asList(16, 26, 36, 46)));
+            mat.add(new ArrayList<Integer>(Arrays.asList(25, 30, 38, 49)));
+            mat.add(new ArrayList<Integer>(Arrays.asList(33, 34, 40, 50)));
+
+            int k = 3;
+            int kthsmall = kthSmallest(mat,k);
+
+            if(kthsmall == -1)
+                System.out.println("3rd smallest element doesn't exist.");
+            else
+                System.out.println("3rd smallest element = "+kthsmall);
+
+        }
+    }
 }
