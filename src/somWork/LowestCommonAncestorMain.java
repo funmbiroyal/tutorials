a9eae61d055c41cfae473e03b8a2538664b6da2b
Grace <funmibiajadi@gmail.com>
2021-10-21 05:11:06 +0100

    Main method

--- src/somWork/LowestCommonAncestorMain.java
+++ src/somWork/LowestCommonAncestorMain.java
@@ -1,2 +1,20 @@
-package somWork;public class LowestCommonAncestorMain {
+package somWork;
+
+import static somWork.LowestCommonAncestor.LCA;
+
+public class LowestCommonAncestorMain {
+    public static void main(String[] args) {
+        // Constructing the BST in above example
+        LowestCommonAncestor.Node root = new LowestCommonAncestor.Node(20);
+        root.left = new LowestCommonAncestor.Node(11);
+        root.right = new LowestCommonAncestor.Node(24);
+        root.right.left = new LowestCommonAncestor.Node(21);
+        root.right.right = new LowestCommonAncestor.Node(35);
+        root.right.right.left = new LowestCommonAncestor.Node(32);
+        root.right.right.right = new LowestCommonAncestor.Node(40);
+        // Queries
+        System.out.println(LCA(root, 24, 40));
+        System.out.println(LCA(root, 11, 21));
+        System.out.println(LCA(root, 32, 40));
+    }
 }
