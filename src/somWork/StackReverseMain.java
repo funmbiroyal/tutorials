e8eb3ffa251d03c80bbbfd9c40f985612de29772
Grace <funmibiajadi@gmail.com>
2021-10-24 12:56:00 +0100

    reversing stack main method

--- src/somWork/StackReverseMain.java
+++ src/somWork/StackReverseMain.java
@@ -1,2 +1,25 @@
-package somWork;public class StackReverseMain {
+package somWork;
+
+import static somWork.StackReverse.reverse;
+import static somWork.StackReverse.stack;
+
+public class StackReverseMain {
+    public static void main(String[] args)
+    {
+
+        stack.push('1');
+        stack.push('2');
+        stack.push('3');
+        stack.push('4');
+
+        System.out.println("Original Stack");
+
+        System.out.println(stack);
+
+        reverse();
+
+        System.out.println("Reversed Stack");
+
+        System.out.println(stack);
+    }
 }
