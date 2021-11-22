ad73af5daffafaec3ef56d6aa7db70bd939a5592
Grace <funmibiajadi@gmail.com>
2021-11-22 09:13:07 +0100

    Card unit tests

--- src/Chapter7/myCardGameTests/CardTest.java
+++ src/Chapter7/myCardGameTests/CardTest.java
@@ -1,2 +1,14 @@
-package Chapter7.myCardGameTests;public class CardTest {
+package Chapter7.myCardGameTests;
+
+import Chapter7.myCardGame.Card;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CardTest {
+    @Test
+    void createCardTest(){
+        Card card = new Card("face","suit");
+        assertNotNull(card);
+    }
 }
