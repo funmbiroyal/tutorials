07d1297d9732fa81b74d4d6fff1cc501609f8a6c
Grace <funmibiajadi@gmail.com>
2021-11-22 09:14:00 +0100

    DeckOfCardsTest

--- src/Chapter7/myCardGameTests/DeckOfCardsTest.java
+++ src/Chapter7/myCardGameTests/DeckOfCardsTest.java
@@ -1,2 +1,36 @@
-package Chapter7.myCardGameTests;public class DeckOfCardsTest {
+package Chapter7.myCardGameTests;
+
+import Chapter7.myCardGame.Card;
+import Chapter7.myCardGame.DeckOfCards;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DeckOfCardsTest {
+    DeckOfCards deckOfCards;
+    Card[] deck;
+    int numberOfCardsInDeck = 52;
+    @BeforeEach
+    void startAllTestWith(){
+        deckOfCards = new DeckOfCards();
+    }
+    @Test
+    void createDeckOfCardsTest(){
+        assertNotNull(deckOfCards);
+    }
+    @Test
+    void canShuffleTest(){
+        DeckOfCards deckOfCards = new DeckOfCards();
+        DeckOfCards deckOfCards2 = new DeckOfCards();
+        deckOfCards.shuffle();
+        assertNotSame(deckOfCards,deckOfCards2);
+    }
+    @Test
+    void canDealCardTest(){
+        deckOfCards.shuffle();
+        
+    }
+
 }
