6f2690bf565d7d501b5b63308486f47a5127ece4
Grace <funmibiajadi@gmail.com>
2021-12-08 11:35:36 +0100

    Deck of cards main method

--- src/Chapter7/myCardGame/DeckOfCardsMain.java
+++ src/Chapter7/myCardGame/DeckOfCardsMain.java
@@ -1,7 +1,20 @@
 package Chapter7.myCardGame;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class DeckOfCardsMain {
     public static void main(String[] args) {
-
+     DeckOfCards myDeckOfCards = new DeckOfCards();
+     myDeckOfCards.shuffle();
+     for(int i = 1; i <= 52; i++){
+         System.out.printf("%-19s",myDeckOfCards.dealCard());
+         if (i % 4==0)
+             System.out.println();
+     }
+        System.out.println();
+        System.out.println("The five poker cards dealt are: ");
+        System.out.println(Arrays.toString(myDeckOfCards.dealHand()));
     }
+
 }
